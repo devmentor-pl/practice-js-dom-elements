307241cb97270a2ff5f79e90a1cd94714862ad91
Dawid <dwilk92@o2.pl>
2020-04-28 11:24:41 +0200

    zad 1 skoczone

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,24 @@
-console.log('DOM');
+
+
+const comments= document.querySelector('.comments');
+//console.log(comments)
+if(comments)
+{
+    const commentsItems= comments.querySelector('.comments__item' && '.comments__item--newest');
+    if(commentsItems){
+        //console.log(commentsItems)
+        const data = commentsItems.querySelectorAll('span');
+        const arr = Array.from(data);
+        
+        if(data)
+       for(let i = 0; i<arr.length; i++)
+      {
+          //console.log(arr[i]);
+          const dataInfo = arr[i].getAttribute('data-info');
+          console.log(dataInfo);  
+ 
+   
+      }
+          console.log(arr.length);
+    }
+}
