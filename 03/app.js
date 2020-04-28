ab473a25813fcce4d24ac271eff1c2151d91ec84
Dawid <dwilk92@o2.pl>
2020-04-28 11:26:07 +0200

    zad 3 - mala podpowiedz

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,13 @@
 console.log('DOM');
+const parent = document.querySelector('.parent-for-button')
+console.log(parent)
+
+const newButton = document.createElement('button');
+//newButton.classList.add('btn')
+//newButton.style.padding = '5px', '20px'
+//parent.appendChild(newButton)
+ 
+
 
 const buttonSettings = {
     attr: {
@@ -12,3 +21,13 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+const a = buttonSettings
+ console.log(a)
+ for(key in buttonSettings)
+ {
+     const o = buttonSettings[key]
+     console.log(buttonSettings[key])
+     //newButton.seto()
+     parent.appendChild(newButton)
+ }
+ 
