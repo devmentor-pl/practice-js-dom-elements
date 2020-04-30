c92511a02430ccf9d992c5bc71ba34bfecc807c7
Mateusz Bogolubow <mateusz@bogolubow.com>
2020-04-30 13:27:10 +0200

    Add array with objects

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+// struktura do wykorzystania w pętli
+const menuItems = [
+    {text: 'start', url: '/'},
+    {text: 'galeria', url: '/gallery'},
+    {text: 'kontakt', url: '/contact'},
+];
