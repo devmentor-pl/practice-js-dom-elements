28d60d454818868e5bbeb3001508c34057e8ee64
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-08 19:02:07 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+const ul = document.getElementsByTagName('ul');
+const a = document.getElementsByTagName('a');
+
+if (ul) {
+    const arrA = Array.from(a);
+    for (let i = 0; i < arrA.length; i++) {
+        var content = a[i].getAttribute('data-url');
+        console.log(content);
+        arrA[i].setAttribute('href', content)
+    }
+    console.log(arrA[3]);
+
+}
