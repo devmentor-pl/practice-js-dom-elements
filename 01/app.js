2f4d0f5d4ec23cc3ff7d28f1f200e91d5975ba86
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-08 18:35:42 +0200

    first task done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,23 @@
 console.log('DOM');
+
+const commentsItem = document.getElementsByClassName('comments__item');
+
+if (commentsItem) {
+    const arr = Array.from(commentsItem);
+    const children = arr[0].children;
+
+    const arrChildren = Array.from(children);
+    console.log(arrChildren);
+
+    for (let i = 0; i < arrChildren.length; i++) {
+        const span = arrChildren[i].children;
+        console.log(span);
+        const arrSpan = Array.from(span);
+        for (let j = 0; j < arrSpan.length; j++) {
+
+            if (arrSpan[j].hasAttribute('data-info')) {
+                console.log(arrSpan[j])
+            }
+        }
+    }
+}
