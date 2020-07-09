8e34d8bd137774f9528de39407ec7ac9db55db64
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-09 14:07:08 +0200

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -5,10 +5,40 @@ const buttonSettings = {
         className: 'btn',
         title: 'super button'
     },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
+    style: {
+        border: '1px solid red',
+        padding: '10px 20px',
+        color: 'blue',
+        background: 'yellow',
+        borderRadius: '5px'
     },
     text: 'Click me!',
+}
+
+
+
+const parent = document.querySelector('section');
+console.log(parent);
+
+if (parent) {
+    const newButton = document.createElement('button');
+
+    for (const property in buttonSettings) {
+        const result = buttonSettings[property];
+
+        if (property === 'attr') {
+            for (const key in result) {
+                newButton[key] = result[key];
+            }
+        } else if (property === 'style') {
+            for (const key in result) {
+                newButton.style[key] = result[key];
+            }
+        } else {
+            newButton.innerText = buttonSettings.text;
+        }
+
+        console.log(newButton);
+    }
+    parent.appendChild(newButton);
 }
