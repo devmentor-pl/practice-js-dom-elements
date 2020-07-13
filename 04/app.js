9a89a2676b023a827dea857a20fb39324221cd95
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-13 09:51:32 +0200

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -2,7 +2,28 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+    { text: 'start', url: '/' },
+    { text: 'galeria', url: '/gallery' },
+    { text: 'kontakt', url: '/contact' },
 ];
+
+const parent = document.querySelector('nav');
+console.log(parent);
+console.log(menuItems.length);
+
+
+if (parent) {
+    const list = document.createElement('ul');
+    console.log(list);
+    parent.appendChild(list);
+    for (let i = 0; i < menuItems.length; i++) {
+        const listItem = document.createElement('li');
+        const a = document.createElement('a');
+        list.appendChild(listItem);
+        listItem.appendChild(a);
+        console.log(menuItems[i]);
+        a.setAttribute('href', menuItems[i].url);
+        a.innerText = menuItems[i].text;
+        console.log(a);
+    }
+}
