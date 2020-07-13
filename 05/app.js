f37ad4c8a5d2c5a4fd6379e5d46fc87b4bed7914
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-13 11:41:36 +0200

    task 5 done

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,29 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+
+if (curr) {
+    const button = document.createElement('button');
+    button.innerText = 'usuń z koszyka';
+    const parent = curr.parentElement;
+    console.log(parent);
+
+    parent.appendChild(button);
+    const children = parent.children;
+    console.log(children);
+    for (let i = 0; i < children.length; i++) {
+        console.log(children[i]);
+        children[i].classList.add('.siblings');
+    }
+    const secondSibling = parent.nextElementSibling;
+    secondSibling.setAttribute('title', 'nextElementSibling');
+    const thirdSibling = secondSibling.nextElementSibling;
+    const childrenThird = thirdSibling.children;
+    const buttonThird = childrenThird[2];
+    console.log(childrenThird);
+    const p = childrenThird[1].cloneNode(true);
+    thirdSibling.insertBefore(p, buttonThird);
+    const lastSibling = parent.cloneNode(true);
+    parent.parentElement.insertBefore(lastSibling, parent);
+}
