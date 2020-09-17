dfc1f7395f1158adc6d27464a87575e7b1caf2f7
AnnaBuller <aniabela@wp.pl>
2020-09-17 18:45:55 +0200

    solve 01 task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,6 @@
-console.log('DOM');
+const twoClassesEl = document.querySelector('.comments__item.comments__item--newest');
+
+if (twoClassesEl) {
+  const hasAttr = twoClassesEl.querySelectorAll("*[data-info]");
+  console.log(hasAttr.length);
+}
