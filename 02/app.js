279e766e15ff93cbd62d8a767f9b14c98007208f
AnnaBuller <aniabela@wp.pl>
2020-09-18 11:39:15 +0200

    solve 02 task

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
-console.log('DOM');
+const hasUrlList = document.querySelectorAll("[data-url]");
+console.log(hasUrlList)
+
+if (hasUrlList) {
+  // Nie zrozumiałam - czy o takie zapisanie wartości atrybutu do zmiennej chodziło? Czy może tylko o tą NodeList wyżej?
+  // const link0 = hasUrlList[0].getAttribute('data-url');
+  // const link1 = hasUrlList[1].getAttribute('data-url');
+  // const link2 = hasUrlList[2].getAttribute('data-url');
+
+  hasUrlList.forEach((item, index) => {
+    item.setAttribute('href', hasUrlList[index].getAttribute('data-url'));
+  })
+}
+
