38a0079b0a794146a811f29e5fc5dcda99b9abf7
AnnaBuller <aniabela@wp.pl>
2020-09-19 13:53:08 +0200

    solve 03 task

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,3 @@
-console.log('DOM');
-
 const buttonSettings = {
     attr: {
         className: 'btn',
@@ -11,4 +9,33 @@ const buttonSettings = {
         color: '#444'
     },
     text: 'Click me!',
+}
+
+const btnParent = document.querySelector('.parent-for-button');
+
+if (btnParent) {
+    const btnNew = document.createElement('button');
+
+    for (item in buttonSettings.attr) {
+        if (typeof buttonSettings.attr === 'undefined') { null } // nie potrafię zdecydować, czy jest tu to potrzebne, bo gdy tego nie ma, a usuwam właściwość attr z obiektu buttonSettings, to nie wyrzuca błędu (w sumie dlaczego?).
+        else {
+            btnNew.className = buttonSettings.attr.className;
+            btnNew.setAttribute('title', buttonSettings.attr.title)
+        }
+    }
+    for (item in buttonSettings.css) {
+        if (typeof buttonSettings.css === 'undefined') { null }
+        else {
+            btnNew.style.border = buttonSettings.css.border
+            btnNew.style.padding = buttonSettings.css.padding
+            btnNew.style.color = buttonSettings.css.color
+        }
+    }
+    if (typeof buttonSettings.text === 'undefined') { null } // tu np. sprawdzenie tego warunku się przydaje, bo inaczej wstawia do buttona tekst 'undefined'.
+    else {
+        btnNew.innerHTML = buttonSettings.text;
+    }
+
+    btnParent.appendChild(btnNew)
+    console.log(btnParent.innerHTML)
 }
