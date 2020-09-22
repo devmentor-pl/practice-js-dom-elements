e6d93180fbd7e07d497edcd1008d8e3093ef21d3
AnnaBuller <aniabela@wp.pl>
2020-09-22 17:02:58 +0200

    solve 05 task

--- 05/app.js
+++ 05/app.js
@@ -18,14 +18,14 @@ if (curr) {
   // dla następnego elementu o klasie .article względem rodzica dla elementu o klasie .js-curr dodaj atrybut title o wartości nextElementSibling
   curr.parentElement.nextElementSibling.setAttribute('title', 'nextElementSibling');
 
-  //do ostatniego artykułu dodaj dodatkowy paragraf i umieść go przed znacznikiem <button/>
+  // do ostatniego artykułu dodaj dodatkowy paragraf i umieść go przed znacznikiem <button/>
   const newPEl = document.createElement('p');
   newPEl.innerText = '====>>> This is a new paragraph.';
   const section = curr.parentElement.parentElement;
   const lastBtn = section.lastElementChild.querySelector('button');
   section.lastElementChild.insertBefore(newPEl, lastBtn);
 
-  //dodaj kolejny artykuł do listy (jako pierwszy), który będzie miał tą samą strukturę co pozostałe artykuły.
+  // dodaj kolejny artykuł do listy (jako pierwszy), który będzie miał tą samą strukturę co pozostałe artykuły.
   const pureArt = section.querySelector('article:nth-child(2)');
   const newArt = pureArt.cloneNode(true);
   newArt.querySelector('h1').innerText = 'Sample Article Title';
