10b25b63d9e753919b171c5547fd5bfea1744016
AnnaBuller <aniabela@wp.pl>
2020-09-22 17:04:50 +0200

    change if stat

--- 03/app.js
+++ 03/app.js
@@ -16,26 +16,21 @@ const btnParent = document.querySelector('.parent-for-button');
 if (btnParent) {
     const btnNew = document.createElement('button');
 
-    if (typeof buttonSettings.attr === 'undefined') { null }
-    else {
+    if (typeof buttonSettings.attr !== 'undefined') {
         for (item in buttonSettings.attr) {
             btnNew.className = buttonSettings.attr.className;
             btnNew.setAttribute('title', buttonSettings.attr.title)
         }
     }
 
-        if (typeof buttonSettings.css === 'undefined') { null }
-        else {
-            for (item in buttonSettings.css) {
-                btnNew.style.border = buttonSettings.css.border
-                btnNew.style.padding = buttonSettings.css.padding
-                btnNew.style.color = buttonSettings.css.color
+    if (typeof buttonSettings.css !== 'undefined') {
+        for (item in buttonSettings.css) {
+            btnNew.style.border = buttonSettings.css.border
+            btnNew.style.padding = buttonSettings.css.padding
+            btnNew.style.color = buttonSettings.css.color
         }
     }
-    if (typeof buttonSettings.text === 'undefined') { null }
-    else {
-        btnNew.innerHTML = buttonSettings.text;
-    }
+    if (buttonSettings.text) { btnNew.innerHTML = buttonSettings.text; }
 
     btnParent.appendChild(btnNew)
     console.log(btnParent.innerHTML)
