57a84df3283b591a8aab716b7d4ba3c9a20ab16d
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-09-22 15:55:46 +0200

    Finish task 4

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,38 @@
-console.log('DOM');
+console.log("DOM");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: "start", url: "/" },
+  { text: "galeria", url: "/gallery" },
+  { text: "kontakt", url: "/contact" },
 ];
+
+const nav = document.querySelector("nav");
+
+if (nav) {
+  const ul = document.createElement("ul");
+
+  //   ul.appendChild(createLinkItem("start", "/"));
+  //   ul.appendChild(createLinkItem("galeria", "/gallery"));
+  //   ul.appendChild(createLinkItem("kontakt", "/contact"));
+
+  menuItems.forEach((item) => {
+    if (item.text !== undefined && item.url !== undefined) {
+      ul.appendChild(createLinkItem(item.text, item.url));
+    }
+  });
+
+  nav.appendChild(ul);
+}
+
+function createLinkItem(text, url) {
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+
+  link.setAttribute("href", url);
+  link.innerText = text;
+
+  li.appendChild(link);
+
+  return li;
+}
