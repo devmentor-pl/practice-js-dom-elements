5233e7f5c4d370ee7f1b5ed3f7c227dd1982a054
Bartosz Białecki <bartosz.bialecki@gmail.com>
2020-09-22 15:38:03 +0200

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
-console.log('DOM');
+const links = document.querySelectorAll("a[data-url]");
+
+if (links) {
+  links.forEach((link) => {
+    const url = link.dataset.url;
+    link.setAttribute("href", url);
+  });
+}
