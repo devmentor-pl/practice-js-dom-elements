fd5e832723b8112927c2210b7d348c8bef7766bd
AnnaBuller <aniabela@wp.pl>
2020-09-22 11:38:04 +0200

    fix forEach loop

--- 02/app.js
+++ 02/app.js
@@ -1,13 +1,7 @@
 const hasUrlList = document.querySelectorAll("[data-url]");
 
-if (hasUrlList) {
-  // Nie zrozumiałam - czy o takie zapisanie wartości atrybutu do zmiennej chodziło? Czy może tylko o tą NodeList wyżej?
-  // const link0 = hasUrlList[0].getAttribute('data-url');
-  // const link1 = hasUrlList[1].getAttribute('data-url');
-  // const link2 = hasUrlList[2].getAttribute('data-url');
+hasUrlList.forEach((item) => {
+  item.setAttribute('href', item.getAttribute('data-url'));
+})
 
-  hasUrlList.forEach((item, index) => {
-    item.setAttribute('href', hasUrlList[index].getAttribute('data-url'));
-  })
-}
 
