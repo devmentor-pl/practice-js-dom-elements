bf4ff52b0d0bdb9671aacdc7309ceb8b165ac566
Michał Pastewski <michpastewski@gmail.com>
2020-09-23 20:35:16 +0200

    solution task_4 and task_5

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,20 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+const ul = document.createElement('ul');
+
+for(let i = 0; i < 3; i++) {
+   ul.appendChild(document.createElement('li'));
+}
+nav.appendChild(ul);
+
+const liList = ul.querySelectorAll('li');
+liList.forEach((li) => li.appendChild(document.createElement('a')));
+
+const linkList = ul.querySelectorAll('a');
+ linkList.forEach((a, index) => {
+   a.innerText = menuItems[index].text;
+   a.setAttribute('href', menuItems[index].url);
+ });
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,20 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const currParent = curr.parentNode;
+console.log(currParent)
+
+const removeButton = document.createElement('button');
+removeButton.innerText = 'usuń z koszyka';
+currParent.appendChild(removeButton);
+
+for(let i = 0; i < currParent.children.length; i++) {
+   currParent.children[i].classList.add('siblings');
+}
+
+currParent.nextElementSibling.setAttribute('title', 'nextElementSibling');
+
+const elementWithNewParagrph = currParent.parentNode.lastElementChild.lastElementChild.insertAdjacentHTML('beforebegin', '<p>DODATKOWY PARAGRAF</p>');
+
+currParent.parentNode.insertAdjacentHTML('afterbegin', 
+'<article><h1>NOWA SEKCJA</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quo quibusdam, nemo neque consequuntur pariatur totam? Facere quaerat molestias hic.</p><button>Kupuje!</button></article>');
