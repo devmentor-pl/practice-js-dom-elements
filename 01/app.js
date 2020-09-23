77027f07ebb0d82cc8b805cbe30b436cdb6372a5
Michał Pastewski <michpastewski@gmail.com>
2020-09-23 20:05:39 +0200

    solution task_1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,5 @@
 console.log('DOM');
+
+const commentsElement = document.querySelector('.comments__item.comments__item--newest');
+const infoElements = commentsElement.querySelectorAll("[data-info]");
+console.log(infoElements.length);
