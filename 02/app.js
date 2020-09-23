8c094d2b81f8d4f0c7ecd61ec57734a660f345a2
Michał Pastewski <michpastewski@gmail.com>
2020-09-23 20:07:31 +0200

    solution task_2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const dataUrl = document.querySelectorAll('a[data-url]');
+datalist = [...dataUrl];
+const urls = datalist.map(data => data.dataset.url);
+
+const addAttribute = (data, index) => data.setAttribute(`href`,`${urls[index]}`);
+dataUrl.forEach(addAttribute);
