a8ea8e45889119e81aa411ee3b788149830e63b7
Grzegorz Błaszczyk <ktotok@gmail.com>
2020-10-12 12:13:03 +0200

    task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,23 @@
 console.log('DOM');
+//const list = document.getElementsByClassName("comments__item comments__item--newest");
+//console.log(list);
+
+//const arr = [].slice.call(list);
+//console.log(arr);
+//const pElements = arr.querySelectorAll('p');
+//const dataInfo = pElements.dataset.info;
+//console.log(dataInfo);
+
+//var dataAttribute = list.getAttribute('data-info');//
+//console.log(dataAttribute);//
+
+
+    //const listSec = document.list.querySelectorAll('[data-info]');
+   // console.log(listSec);//
+
+   //var elems = document.querySelectorAll(".comments__item > [data-info]")
+   //console.log(elems.length)
+   //Array.from(elems).forEach( function (el) {
+      //console.log(el.getAttribute("data-info"))
+   //});
+
