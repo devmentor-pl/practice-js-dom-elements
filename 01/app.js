a78c192cae4e55142dc126088cc33e37e6d4c2c2
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-15 17:29:16 +0200

    Search for data-info attr in Task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const commentsEl = document.querySelector('.comments__item, .comments__item--newest');
+
+if (commentsEl) {
+    const dataInfoArray = commentsEl.querySelectorAll('[data-info]');
+    console.log('Ilość znalezionych elementów: ' + dataInfoArray.length);
+}
