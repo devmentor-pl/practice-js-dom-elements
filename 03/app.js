eacf09dadee4d411fb27358971ff5bf4f1c135d7
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-15 18:38:31 +0200

    Create styled button

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,5 @@
 console.log('DOM');
+const section = document.querySelector('section');
 
 const buttonSettings = {
     attr: {
@@ -12,3 +13,39 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+
+
+
+const appendStyledButton = function (btn) {
+    buttonAttr(btn);
+    buttonStyles(btn);
+    buttonText(btn);
+    section.appendChild(btn);
+}
+
+
+const buttonAttr = function (btn) {
+    for (const property in buttonSettings['attr']) {
+        if (property === 'className') {
+            btn.setAttribute('class', buttonSettings['attr'][property]);
+        } else {
+            btn.setAttribute('data-' + property, buttonSettings['attr'][property])
+        }
+    }
+}
+
+const buttonStyles = function (btn) {
+    for (const property in buttonSettings['css']) {
+        btn.style.setProperty(property, buttonSettings['css'][property])
+    }
+}
+
+const buttonText = function (btn) {
+    for (const property in buttonSettings['text']) {
+        btn.textContent = buttonSettings['text'];
+    }
+}
+
+const newButton = document.createElement('button');
+appendStyledButton(newButton);
