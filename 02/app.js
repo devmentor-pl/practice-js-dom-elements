b28a934fd44870d843d01398773a1d1530291728
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-15 17:41:26 +0200

    Create href links from data-url

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,12 @@
 console.log('DOM');
+
+const dataUrlEl = document.querySelectorAll('[data-url]');
+if (dataUrlEl) {
+    dataUrlEl.forEach(function (element) {
+
+        let attrName = element.getAttribute('data-url');
+
+        element.setAttribute('href', attrName);
+        //Działa, ale magia :D 
+    })
+}
