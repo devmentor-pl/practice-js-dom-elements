e218c9ce3ebe06672a91b8a20bf6b323d2edb097
Marek Mitko <mitko.email@gmail.com>
2020-12-31 00:21:45 +0100

    added ex. 4 part 2

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,23 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+// const navMenu = [...document.getElementsByTagName('nav')];
+//?dev sprawdź czemu to nie działa dla ??? : 
+// const navMenu = document.getElementsByTagName('nav');
+const navMenu = document.querySelector('nav');
+console.log(navMenu);
+
+const ulEl = document.createElement('ul');
+menuItems.forEach(function(item){
+    const liEl = document.createElement('li');
+    const aEl = document.createElement('a');
+    aEl.setAttribute('href', item.url);
+    aEl.textContent = item.text;
+    liEl.appendChild(aEl);
+    ulEl.appendChild(liEl);
+
+    console.log(ulEl)
+});
+
+navMenu.appendChild(ulEl);
