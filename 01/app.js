89ab676bc5fd2577c10b777a677975c6d1dab01b
Marek Mitko <mitko.email@gmail.com>
2020-12-30 18:16:45 +0100

    ex. 2 is done

--- 01/app.js
+++ 01/app.js
@@ -2,10 +2,16 @@
 const elCommentsNewest = document.getElementsByClassName('comments__item comments__item--newest');
 console.log(elCommentsNewest);
 
+
+const hasDataInfo = elCommentsNewest.hasAttribute('data-info');
+console.log(hasDataInfo);
+//notE Chyba źle to robie bo robie to na rodzicu a to jest w dzieciach ? 
+
 //?dev Nie wiem czemu się to nie chce wyświetlić 
 const datasetInfo = elCommentsNewest.dataset.info;
 console.log(datasetInfo);
 console.log(elCommentsNewest.dataset.info);
+
 
 
 
--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+const aElements = [...document.getElementsByTagName('a')];
+
+aElements.forEach(function(item){
+    if(item.getAttribute('data-url')){
+        let webAddress = item.getAttribute('data-url');
+        item.setAttribute('href', webAddress);
+    };
+});
