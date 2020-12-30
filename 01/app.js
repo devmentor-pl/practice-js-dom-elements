3f508f84fe356fd6fdeda53be08dfcc3c674a5fb
Marek Mitko <mitko.email@gmail.com>
2020-12-30 17:04:12 +0100

    ex 1 to check

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,23 @@
-console.log('DOM');
+//?dev Czy ten zapis dałoby się jakoś skrócić? Czy w nawiasie tej metody możemy jakoś zapisać dwie klasy jednoczenie? W querySelector by przeszło ale to działa szybciej. 
+const elCommentsNewest = document.getElementsByClassName('comments__item comments__item--newest');
+console.log(elCommentsNewest);
+
+//?dev Nie wiem czemu się to nie chce wyświetlić 
+const datasetInfo = elCommentsNewest.dataset.info;
+console.log(datasetInfo);
+console.log(elCommentsNewest.dataset.info);
+
+
+
+
+
+
+
+
+
+
+// const elCommentsNewest = document.querySelectorAll('.comments__item.comments__item--newest');
+// console.log(elCommentsNewest);
+
+// const datasetInfo = elCommentsNewest.dataset.info;
+// console.log(datasetInfo);
