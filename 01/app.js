8bdf1aa7af5e6d4909eef0340a80e1a1a6d0a661
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-07 21:41:51 +0100

    Zad 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const comment = document.querySelector('.comments__item.comments__item--newest');
+
+console.log(comment);
+
+const attributes = comment.querySelectorAll('[data-info]');
+console.log(attributes.length);
