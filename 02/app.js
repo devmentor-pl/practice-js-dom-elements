71484bc4354adcf691b1290c9d0ae94ccc050173
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-07 21:44:23 +0100

    Zad 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('[data-url]');
+
+links.forEach(function (link) {
+    const href = link.getAttribute('data-url');
+    link.setAttribute('href', href);
+});
+console.log(links);
