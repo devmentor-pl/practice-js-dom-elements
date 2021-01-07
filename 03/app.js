f8e375c02378ec9917d6f942689c49f8f684060c
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-08 00:33:41 +0100

    Zad 03 part 1

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,15 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const buttonParent = document.querySelector('.parent-for-button');
+
+const button = document.createElement('button');
+
+for (const property in buttonSettings) {
+   if(property === 'attr') {
+       const attrObj = buttonSettings[property];
+       console.log(property);
+       console.log(attrObj);
+   }
+}
