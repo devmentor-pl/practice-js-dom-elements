868e964e6690375803f12fa33b32e102b257c5f1
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-08 13:11:22 +0100

    Zad 03 part 2

--- 03/app.js
+++ 03/app.js
@@ -18,9 +18,25 @@ const buttonParent = document.querySelector('.parent-for-button');
 const button = document.createElement('button');
 
 for (const property in buttonSettings) {
-   if(property === 'attr') {
-       const attrObj = buttonSettings[property];
-       console.log(property);
-       console.log(attrObj);
-   }
+
+    if (property === 'attr') {
+        const attrObj = buttonSettings[property];
+        //    console.log(property);
+        //    console.log(attrObj);
+        attrObj.className = 'btn';
+        attrObj.title = 'super button';
+        
+        if (property === 'css') {
+            attrObj.style.border = '1px solid #336699';
+            attrObj.style.padding = '5px 20px';
+            attrObj.color = '#444';
+
+            if (property === 'text') {
+                attrObj.innerText = 'Click me!'
+            }
+        }
+    }
+
+
+
 }
