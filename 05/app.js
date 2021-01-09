f024d70b65089a0784d45a9eb98bef5946459805
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-09 14:08:26 +0100

    Zad 05 a,b

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,24 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+// zadanie 1
+const  btn = document.createElement('button');
+btn.innerText = 'Usuń z koszyka';
+curr.parentElement.appendChild(btn);
+
+//zadanie 2
+
+const children = curr.parentElement.children;
+
+for( let i=0; i < children.length; i++) {
+    if(children[i] !== curr) {
+        children[i].classList.add('siblings');
+    }
+}
+
+
+
+const sibling = curr.parentElement.nextElementSibling;
+
+sibling.setAttribute('title', 'nextElementSibling');
