743389bea9cd0a15bfcd2b109465b4268e51f6f1
GrzegorzGozdziak <ggozdziak@gmail.com>
2021-03-16 19:16:56 +0100

    fourth exercise

--- 04/app.js
+++ 04/app.js
@@ -2,7 +2,14 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+	{ text: 'start', url: '/' },
+	{ text: 'galeria', url: '/gallery' },
+	{ text: 'kontakt', url: '/contact' }
 ];
+
+let nav = document.querySelector('nav');
+
+let ul = document.createElement('ul');
+ul.innerHTML = menuItems.map(({ text, url }) => `<li><a href="${url}">${text}</a></li>`).join('');
+
+nav.appendChild(ul);
