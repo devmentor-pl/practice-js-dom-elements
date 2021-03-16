c00844e656c5c78c19cc24ab8eaa9609f405080a
GrzegorzGozdziak <ggozdziak@gmail.com>
2021-03-16 16:50:34 +0100

    first exercise

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,6 @@
 console.log('DOM');
+
+let element = document.querySelector('.comments__item.comments__item');
+let element_with_data_info = element.querySelectorAll('[data-info]');
+
+console.log(element_with_data_info.length);
