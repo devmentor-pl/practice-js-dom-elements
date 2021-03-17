3639e0ae6550e88b2b0a433d86df9c7b9d0ad649
Agnieszka Polińska <agapoli@gmail.com>
2021-03-17 19:28:17 +0100

    Answers

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,12 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('[data-url]');
+
+console.log(links);
+
+links.forEach(link => {
+    let linkUrl = link.dataset.url;
+    link.href = linkUrl;
+    console.log(link);
+})
+
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,39 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const currParent = curr.parentElement;
+const currSiblings = currParent.children;
+const nextSiblingOfParent = currParent.nextElementSibling;
+const section = currParent.parentElement;
+const lastArticle = section.lastElementChild;
+
+//1. create removeBtn 
+const removeBtn = document.createElement('button');
+removeBtn.textContent = 'usuń z koszyka';
+curr.after(removeBtn);
+
+
+//2. add className .siblings
+for (let i=0; i<currSiblings.length; i++) {
+    if (!currSiblings[i].classList.contains('js-curr')) {
+        currSiblings[i].classList.add('siblings')
+    } 
+}
+
+//3. nextElementSibling of parent with className '.article'
+while (nextSiblingOfParent) {
+    if(nextSiblingOfParent.classList.contains('article')) {
+        nextSiblingOfParent.setAttribute('title', 'nextElementSibling');
+        break;
+    }
+    nextSiblingOfParent = nextSiblingOfParent.nextElementSibling;
+}
+
+//4. add paragraph to the last article
+const p = document.createElement('p');
+lastArticle.lastElementChild.before(p);
+
+//5. add newArticle
+const newArticle = currParent.cloneNode(true)
+section.prepend(newArticle)
+
