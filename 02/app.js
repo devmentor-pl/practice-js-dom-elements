f4a027ec4a622389df8de21f23b6736ed2a12512
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-24 22:17:31 +0200

    Task 02
    Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,20 @@
 console.log('DOM');
+
+// Kolejnym wyzwaniem będzie wyszukanie wszystkich linków, które posiadają atrybut `data-url`.
+
+const dataUrl = document.querySelectorAll('[data-url]');
+console.log(dataUrl)
+
+dataUrl.forEach((el) => {
+
+// Następnie należy pobrać zawartość tego atrybutu do zmiennej.
+
+   const urlVariable = el.getAttribute('data-url');
+   console.log(urlVariable)
+
+// Potem wartość tej zmiennej przypisać do atrybutu `href` wyszukanego elementu `</a>`.
+
+   el.setAttribute('href', urlVariable);
+   console.log(el)
+
+});
