77bb10756c963f8b74759f5713c24a8b9102fcdd
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-24 21:54:22 +0200

    Task 01
    Task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,13 @@
 console.log('DOM');
+
+// Wyszukaj element, który posiada dwie klasy tj. `.comments__item` oraz `.comments__item--newest`
+
+const commentsItem = document.querySelector('.comments__item.comments__item--newest');
+
+// Następnie przeszukaj ten element względem znaczników, które posiadają atrybut `data-info`.
+
+const dataInfo = commentsItem.querySelectorAll('[data-info]');
+
+console.log(commentsItem)
+console.log(dataInfo)
+console.log(`Znaleziono ${dataInfo.length} elementy z atrybutem data-info`);
