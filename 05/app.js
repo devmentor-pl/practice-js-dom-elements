138bd91543ec59aea31ebb75f442df679c6e8d51
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-04-26 19:15:02 +0200

    Task 05

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,32 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+if(curr) {
+    const articleElement = curr.parentElement;
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = "Usuń z koszyka";
+    articleElement.appendChild(buttonElement);
+
+    if(articleElement.hasChildNodes()) {
+        const child = articleElement.children;
+        const arrChild = Array.from(child);
+        arrChild.forEach(function(element) {
+            element.classList.add("siblings");
+        });
+    };
+
+    const siblingsElement = articleElement.nextElementSibling;
+    siblingsElement.setAttribute("title", "nextElementSibling");
+
+    const newParagraph = document.createElement("p");
+    newParagraph.innerText = "Nowy paragraf";
+    const lastArticle = siblingsElement.parentElement.lastElementChild;
+    const buttonArticle = lastArticle.querySelector("button");
+    lastArticle.insertBefore(newParagraph, buttonArticle);
+
+    const newArticle = lastArticle.cloneNode(true);
+    articleElement.parentElement.insertBefore(newArticle, articleElement);
+
+}
+
