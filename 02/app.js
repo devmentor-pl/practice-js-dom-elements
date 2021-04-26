b1b27d085515f6f6bbd12cb01f63077315278ce6
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-04-26 16:05:29 +0200

    Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,13 @@
 console.log('DOM');
+
+const dataUrl = document.querySelectorAll("[data-url");
+
+dataUrl.forEach(function(item) {
+
+    const link = item.getAttribute("data-url")
+    console.log(link);
+
+    item.setAttribute("href", link);
+    console.log(item);
+
+})
