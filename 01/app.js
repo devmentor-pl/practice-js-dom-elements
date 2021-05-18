c7bd6900aa19551e83dafec4ee574070164e3ece
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-05-18 02:00:00 +0200

    Task 01 - done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const element = document.getElementsByClassName('comments__item' && 'comments__item--newest');
+
+
+if(element) {
+    const childrenWithDataAttribute = element[0].querySelectorAll('[data-info]');
+    console.log(childrenWithDataAttribute);
+}
