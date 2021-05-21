b5ed74faec23f3e37c2e8ea584b570664818034d
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-05-21 17:29:59 +0200

    I ended task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,24 @@
 console.log('DOM');
+
+
+// Kolejny wyzwaniem będzie wyszukanie wszystkich linków, które posiadają atrybut `data-url`.
+
+// Następnie należy pobrać zawartość tego atrybutu do zmiennej.
+
+// Potem wartość tej zmiennej przypisać do atrybutu `href` wyszukanego elementu `</a>`.
+
+// Sprawdź czy każdy z linków działa prawidłowo tj. przekierowuje na odpowiednie strony. Zwróć uwagę na ostatni element na liście - on nie powinien Cię nigdzie przekierować.
+
+
+
+
+const linkList = document.querySelectorAll( '[data-url]' );
+
+if(linkList) {
+
+    linkList.forEach( function(element) {
+
+        const elementContent = element.getAttribute('data-url');
+        element.setAttribute('href' , elementContent);
+    } )
+}
