c911bbc37ad44b5308c7447e8711cb94ea0297a9
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-05-22 13:47:58 +0200

    i Ended task 03

--- 03/app.js
+++ 03/app.js
@@ -8,7 +8,27 @@ const buttonSettings = {
     css: {
         border: '1px solid #336699',
         padding: '5px 20px',
-        color: '#444'
+        color: '#444',
+        cursor: 'pointer'
     },
     text: 'Click me!',
+}
+
+const parent = document.querySelector('.parent-for-button')
+
+if(parent) {
+
+   const btn = document.createElement('button');
+
+   for(const key in buttonSettings.attr) {
+    btn.setAttribute( key , buttonSettings.attr[key] )
+   }
+
+   for(const key in buttonSettings.css) {
+    btn.style.setProperty( key , buttonSettings.css[key])
+   }
+
+    btn.textContent = buttonSettings.text;
+
+    parent.appendChild(btn)
 }
