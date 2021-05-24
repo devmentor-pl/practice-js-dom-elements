72e9b905d5fb67a4d240b29fa00cfdf789b0c05b
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-05-24 15:59:49 +0200

    I ended task 04

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,68 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+
+// Sposób drugi
+
+const nav = document.querySelector('nav');
+console.log(nav);
+
+if(nav) {
+    const ul = document.createElement('ul');
+
+    if(ul) {
+        for(let i=1; i<=3; i++) {
+            ul.appendChild(  document.createElement('li') );
+        }
+    }
+
+    nav.appendChild(ul)
+    nav.childNodes[1].remove();
+
+    document.querySelectorAll('li').forEach( function(element, index) {
+
+        element.textContent = menuItems[index].text;
+        element.setAttribute( 'url', menuItems[index].url );
+    } )
+}
+
+
+
+// Sposób pierwszy
+
+// const nav = document.querySelector('nav');
+// console.log(nav)
+
+// if(nav) {
+
+//     const ul = document.createElement('ul')
+
+//     if(ul) {
+
+//         const li = document.createElement('li');
+//         const liSecond = document.createElement('li');
+//         const liThird = li.cloneNode(false);
+
+//         li.setAttribute('href' , '/');
+//         liSecond.setAttribute('href' , '/gallery');
+//         liThird.setAttribute('href' , '/contact');
+
+//         li.textContent = 'start'
+//         liSecond.textContent = 'galeria'
+//         liThird.textContent = 'kontakt'
+
+//         ul.appendChild(li);
+//         ul.appendChild(liSecond);
+//         ul.appendChild(liThird);
+//     }
+
+//     nav.appendChild(ul)
+
+//     nav.childNodes[1].remove();
+// }
+
+
+
+
+
