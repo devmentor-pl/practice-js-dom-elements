c26d93d111fa91502aa5c69123ee5f25b9a30e9a
Maciej <maciej.zadylak@gmail.com>
2021-05-26 12:34:12 +0200

    Tasks 01/02

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+let comments = document.getElementsByClassName('comments__item comments__item--newest');
+comments = [...comments];
+const a = comments[0].querySelectorAll('[data-info]');
+console.log(a.length);
+//
+const comment = document.querySelector('.comments__item.comments__item--newest');
+console.log(comment.querySelectorAll('[data-info]').length);
--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+const dataUrl = document.querySelectorAll('[data-url]');
+if(dataUrl) {
+    dataUrl.forEach(function(el) {
+        const href = el.getAttribute('data-url');
+        el.setAttribute('href',href);
+        console.log(el);
+    })
+}
