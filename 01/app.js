f286f6b5d8638e3c1c3dea2ced54773905e7600e
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-06-04 21:18:08 +0200

    done task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const liEl = document.qetElementsByClassName ('comments__item comments__item--newest');
+
+
+if (liEl) {
+    const dataEl = liEl.querySelectorAll('data-info');
+    for( let i = 0; i < dataEl.length; i++);
+}
+console.log(liEl);
+console.log(dataEl)
