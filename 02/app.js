7f640521e94915843121f953e013274d65bb43fd
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-11 13:07:23 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
-console.log('DOM');
+const dataUrlList = document.querySelectorAll('[data-url]');
+
+if (dataUrlList) {
+    dataUrlList.forEach(function (element) {
+        const link = element.getAttribute('data-url');
+        element.setAttribute('href', link);
+    })
+}
