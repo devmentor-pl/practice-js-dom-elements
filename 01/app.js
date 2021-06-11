9eab32273a547fa08a9dc5a4168985b96cc37b02
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-11 12:52:09 +0200

    Add alternative solution

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,3 @@
-console.log('DOM');
 const commentsItemNewestList = document.querySelectorAll('.comments__item.comments__item--newest'); // uzyłam querySelectorAll by sprawdzić czy nie ma więcej takich elementów z podwójną klasą
 if (commentsItemNewestList) {
     commentsItemNewestList.forEach(function (element) {
@@ -6,3 +5,13 @@ if (commentsItemNewestList) {
         console.log(`liczba znaczników z atrybutem [data-info]: ${dataInfoList.length}`)
     })
 }
+
+//  ---------Czy lepiej z getElementsByClassName? Szybsze ale starsze rozwiąznaie?---- 
+
+// const commentsItemNewestList = [...document.getElementsByClassName('comments__item comments__item--newest')]; 
+// if (commentsItemNewestList) {
+//     commentsItemNewestList.forEach(function (element) {
+//         const dataInfoList = element.querySelectorAll('[data-info]');
+//         console.log(`liczba znaczników z atrybutem [data-info]: ${dataInfoList.length}`)
+//     });
+// }
