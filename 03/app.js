c1462d2a117934f5ef4fc0ad1274d049b9652cbc
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-06-13 22:33:59 +0200

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,32 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+// 1 wyszukaj element rodzica
+const parentElement = document.querySelector('.parent-for-button');
+
+if(parentElement) {
+    // 2 stwórz element button
+    const btn = document.createElement('button');
+    
+    // pobierz potrzebne właściwości, content i atrybuty elementu
+    let attr = buttonSettings.attr
+    let css = buttonSettings.css
+    let text = buttonSettings.text
+    
+    // dodanie contentu
+    btn.textContent = text;
+    // dodanie klasy
+    btn.classList.add(attr.className);
+    // dodanie atrybutu
+    btn.setAttribute('title', attr.title);
+
+    // dodanie styli
+    for (let key in css) {
+        btn.style.setProperty(key, css[key])
+    }
+    
+    // 3 dodanie elementu do HTML
+    parentElement.appendChild(btn);
+    }
+    
