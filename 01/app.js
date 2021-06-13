6c16cb48b179e54c3933876af68177cf90842305
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-06-13 20:31:06 +0200

    add first task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const el1 = document.querySelector(".comments__item" && ".comments__item--newest")
+
+if(el1) {
+  let arrOfElements = el1.querySelectorAll('span')
+  arrOfElements.forEach(item => item.hasAttribute('data-info'))
+  console.log(arrOfElements.length)
+}
