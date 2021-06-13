798e0bebf6f0cf65fe2a3d06e670bf2eae090ed5
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-06-13 20:55:56 +0200

    task two finished

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,13 @@
 console.log('DOM');
+
+const arrOfLinks = document.querySelectorAll('a[data-url]')
+console.log(arrOfLinks.length)
+if(arrOfLinks && arrOfLinks.length !== 0) {
+  let x = arrOfLinks[0].getAttribute('data-url'); 
+  let y = arrOfLinks[1].getAttribute('data-url'); 
+  let z = arrOfLinks[2].getAttribute('data-url');
+  arrOfLinks[0].setAttribute('href', `${x}`)
+  arrOfLinks[1].setAttribute('href', `${y}`)
+  arrOfLinks[2].setAttribute('href', `${z}`)
+  arrOfLinks.forEach(item => item.setAttribute('target', '_blank'))
+}
