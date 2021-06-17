5ce356241f5bd6fbad79bc46cef917a04145567a
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-06-17 06:01:16 +0200

    first option to add HTML structure

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,47 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+// Bez forEach
+// dodanie ul do nav
+const navigation = document.querySelector('nav')
+const unOrderedList = document.createElement('ul');
+const liElOne = document.createElement('li');
+
+
+
+if(navigation) {
+    navigation.appendChild( unOrderedList )
+    
+    if(unOrderedList) {
+        unOrderedList.appendChild(liElOne)
+
+        if(liElOne) {
+            const cloneElTwo = liElOne.cloneNode(true)
+            const cloneElThree = liElOne.cloneNode(true)
+
+            unOrderedList.appendChild(cloneElTwo)
+            unOrderedList.appendChild(cloneElThree)
+
+            if(cloneElTwo && cloneElThree) {
+                const linkElement = document.createElement('a');
+                linkElement.setAttribute('href', '/');
+
+                cloneLinkTwo = linkElement.cloneNode(true);
+                cloneLinkThree = linkElement.cloneNode(true);
+
+                liElOne.appendChild(linkElement).textContent = "home";
+                cloneElTwo.appendChild(cloneLinkTwo).textContent = "gallery"
+                cloneElThree.appendChild(cloneLinkThree).textContent = "contact"
+
+                if(cloneLinkTwo && cloneElThree) {
+                    cloneLinkTwo.setAttribute('href', '/gallery');
+                    cloneLinkThree.setAttribute('href', '/contact');
+                } 
+            }
+        }
+    }
+
+}
+
+
