11df63e7c1c2b427a1d2ceacf7d385e2e1f19e8e
Krzysiek Swol <64749798+krzysiekswol@users.noreply.github.com>
2021-06-21 22:01:02 +0200

    add forEach

--- 04/app.js
+++ 04/app.js
@@ -15,38 +15,62 @@ const liElOne = document.createElement('li');
 
 
 
-if(navigation) {
-    navigation.appendChild( unOrderedList )
+// if(navigation) {
+//     navigation.appendChild( unOrderedList )
     
-    if(unOrderedList) {
-        unOrderedList.appendChild(liElOne)
+//     if(unOrderedList) {
+//         unOrderedList.appendChild(liElOne)
 
-        if(liElOne) {
-            const cloneElTwo = liElOne.cloneNode(true)
-            const cloneElThree = liElOne.cloneNode(true)
+//         if(liElOne) {
+//             const cloneElTwo = liElOne.cloneNode(true)
+//             const cloneElThree = liElOne.cloneNode(true)
 
-            unOrderedList.appendChild(cloneElTwo)
-            unOrderedList.appendChild(cloneElThree)
+//             unOrderedList.appendChild(cloneElTwo)
+//             unOrderedList.appendChild(cloneElThree)
 
-            if(cloneElTwo && cloneElThree) {
-                const linkElement = document.createElement('a');
-                linkElement.setAttribute('href', '/');
+//             if(cloneElTwo && cloneElThree) {
+//                 const linkElement = document.createElement('a');
+//                 linkElement.setAttribute('href', '/');
 
-                cloneLinkTwo = linkElement.cloneNode(true);
-                cloneLinkThree = linkElement.cloneNode(true);
+//                 cloneLinkTwo = linkElement.cloneNode(true);
+//                 cloneLinkThree = linkElement.cloneNode(true);
 
-                liElOne.appendChild(linkElement).textContent = "home";
-                cloneElTwo.appendChild(cloneLinkTwo).textContent = "gallery"
-                cloneElThree.appendChild(cloneLinkThree).textContent = "contact"
+//                 liElOne.appendChild(linkElement).textContent = "home";
+//                 cloneElTwo.appendChild(cloneLinkTwo).textContent = "gallery"
+//                 cloneElThree.appendChild(cloneLinkThree).textContent = "contact"
 
-                if(cloneLinkTwo && cloneElThree) {
-                    cloneLinkTwo.setAttribute('href', '/gallery');
-                    cloneLinkThree.setAttribute('href', '/contact');
-                } 
-            }
-        }
-    }
+//                 if(cloneLinkTwo && cloneElThree) {
+//                     cloneLinkTwo.setAttribute('href', '/gallery');
+//                     cloneLinkThree.setAttribute('href', '/contact');
+//                 } 
+//             }
+//         }
+//     }
 
+// }
+
+// z ForEach 
+
+const unOrderedListSecond = document.createElement('ul');
+navigation.appendChild(unOrderedListSecond);
+
+if (unOrderedListSecond) {
+
+    menuItems.forEach((item) => {
+        const liElement = document.createElement('li');
+        liElement.setAttribute('href', `${item.url}`)
+        liElement.textContent = `${item.text}`;
+        unOrderedListSecond.appendChild(liElement);
+        
+    })
+    
 }
+
+
+
+
+
+
+
 
 
