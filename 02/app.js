a68dc4800e796b3b44368d464ae63f9fef8baae8
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-06-29 19:09:38 +0200

    use forEach method

--- 02/app.js
+++ 02/app.js
@@ -3,11 +3,8 @@ console.log('DOM');
 const arrOfLinks = document.querySelectorAll('a[data-url]')
 console.log(arrOfLinks.length)
 if(arrOfLinks && arrOfLinks.length !== 0) {
-  let x = arrOfLinks[0].getAttribute('data-url'); 
-  let y = arrOfLinks[1].getAttribute('data-url'); 
-  let z = arrOfLinks[2].getAttribute('data-url');
-  arrOfLinks[0].setAttribute('href', `${x}`)
-  arrOfLinks[1].setAttribute('href', `${y}`)
-  arrOfLinks[2].setAttribute('href', `${z}`)
-  arrOfLinks.forEach(item => item.setAttribute('target', '_blank'))
+  arrOfLinks.forEach(item => {
+    item.setAttribute('href', `${item.getAttribute('data-url')}`);
+    item.setAttribute('target', '_blank')
+  })
 }
