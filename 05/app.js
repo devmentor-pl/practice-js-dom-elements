e07aea076db57857bfb53f3e108011b72042552c
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-08-04 18:04:25 +0200

    closes task 5

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,24 @@
-console.log('DOM');
-
 const curr = document.querySelector('.js-curr');
+const currParent = curr.parentElement;
+
+const btn = document.createElement('button');
+btn.innerText = 'usuń z koszyka';
+currParent.appendChild(btn);
+
+const childrenList = currParent.children;
+for (let i = 0; i < childrenList.length; i++) {
+    if (!childrenList[i].classList.contains('js-curr')) {
+        childrenList[i].classList.add('siblings');
+    }
+}
+
+currParent.nextElementSibling.setAttribute('title', 'nexElementSibling');
+
+const lastArticle = currParent.parentElement.lastElementChild;
+const paragraph = document.createElement('p');
+paragraph.innerText = 'Test';
+const lastArticleBtn = lastArticle.lastElementChild;
+lastArticle.insertBefore(paragraph, lastArticleBtn);
+
+const firstArticle = lastArticle.cloneNode(true);
+currParent.parentElement.insertBefore(firstArticle, currParent);
