adc615d06bfd2620b0f27008509da14d56ecda00
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-12 11:54:52 +0200

    Finish task 01 and 02

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
-console.log('DOM');
+console.log("DOM");
+
+const comment = document.querySelector(
+  ".comments__item.comments__item--newest"
+);
+
+const infos = comment.querySelectorAll("[data-info");
+
+console.log(infos.length);
--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,7 @@
-console.log('DOM');
+console.log("DOM");
+
+const links = document.querySelectorAll("a[data-url]");
+
+links.forEach(function (el) {
+  el.setAttribute("href", el.dataset.url);
+});
