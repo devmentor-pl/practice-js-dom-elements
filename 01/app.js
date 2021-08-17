49c8eb1143c7cfc608c83ffbf1124d388c10beb9
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-17 12:11:30 +0200

    Fix

--- 01/app.js
+++ 01/app.js
@@ -4,8 +4,8 @@ const comment = document.querySelector(
   ".comments__item.comments__item--newest"
 );
 
-const infos = comment.querySelectorAll("[data-info");
+if (comment !== null) {
+  const infos = comment.querySelectorAll("[data-info");
 
-if (infos !== null) {
   console.log(infos.length);
 }
