cfa45348cdcfeac1bbc2cd4d0fd5fec269562555
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-16 20:52:39 +0200

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const attributeInLinks = document.querySelectorAll('[data-url]');
+console.log(attributeInLinks);
+
+[...attributeInLinks].forEach((el) =>
+	el.setAttribute('href', el.getAttribute('data-url'))
+);
