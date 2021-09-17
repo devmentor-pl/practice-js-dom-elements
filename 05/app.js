5e09ea271701c75de192f3ca11cd69a50a6ddbb8
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-17 21:32:30 +0200

    Finish task 5

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,54 @@
-console.log('DOM');
-
 const curr = document.querySelector('.js-curr');
+
+const btnSibling = document.createElement('button');
+btnSibling.textContent = 'Usuń z koszyka';
+
+// 1.
+curr.parentNode.insertBefore(btnSibling, curr.nextSibling);
+
+// 2.
+function newClassForSiblings(element, parent) {
+	const children = [...parent.children];
+	const filteredSiblings = children.filter((child) => child !== element);
+	filteredSiblings.map((el) => el.classList.add('siblings'));
+}
+newClassForSiblings(curr, curr.parentElement);
+
+// 3.
+curr.parentElement.nextElementSibling.setAttribute(
+	'title',
+	'nextElementSibling'
+);
+
+// 4.
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'New Content for paragraph';
+
+const lastArticle =
+	curr.parentElement.nextElementSibling.nextElementSibling.children;
+
+lastArticle[1].parentNode.insertBefore(
+	newParagraph,
+	lastArticle[1].nextSibling
+);
+
+// 5.
+const sectionParent = curr.parentElement;
+
+const newArticle = document.createElement('article');
+sectionParent.parentNode.insertBefore(newArticle, sectionParent);
+
+const newSectionParent = curr.parentElement.previousElementSibling;
+console.log(newSectionParent);
+
+const newHeading = document.createElement('h2');
+newHeading.textContent = 'JS new heading';
+newSectionParent.appendChild(newHeading);
+
+const anotherParagraph = document.createElement('p');
+anotherParagraph.textContent = `It's fun to use DOM.`;
+newSectionParent.appendChild(anotherParagraph);
+
+const anotherBtn = document.createElement('button');
+anotherBtn.textContent = `I'm buying it!`;
+newSectionParent.appendChild(anotherBtn);
