b71e141234e6f51eaf583086325607ebf73ac790
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-20 12:28:26 +0200

    Correct task 1

--- 01/app.js
+++ 01/app.js
@@ -4,5 +4,5 @@ const element = document.querySelector('.comments__item.comments__item--newest')
 console.log(element);
 
 const smth = element.querySelectorAll('[data-info]');
-console.log(smth);
+console.log(smth.length);
 
