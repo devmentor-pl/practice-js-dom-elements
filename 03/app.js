95b720abcc406617442e75757239d7db5fffae70
Grzegorz Chrymecki <grzegorzchrymecki@gmail.com>
2021-09-26 11:23:01 +0100

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,20 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const button = document.createElement('button');
+const htmlHook = document.getElementsByClassName('parent-for-button');
+
+for(const props in buttonSettings){
+    if(typeof buttonSettings[props] === 'object'){
+        if(buttonSettings[props].className !== undefined) button.classList.add(buttonSettings[props].className);
+        if(buttonSettings[props].title !== undefined) button.setAttribute('title', buttonSettings[props].title);
+        if(buttonSettings[props].border !== undefined) button.style.border = buttonSettings[props].border;
+        if(buttonSettings[props].padding !== undefined) button.style.padding = buttonSettings[props].padding;
+        if(buttonSettings[props].color !== undefined) button.style.color = buttonSettings[props].color;
+    }else{
+        button.textContent = buttonSettings[props];
+    }
+}
+
+htmlHook[0].appendChild(button);
