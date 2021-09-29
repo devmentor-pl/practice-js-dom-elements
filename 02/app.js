071d2254d59e8a4444203af791f55b9c5edefbda
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-29 16:26:19 +0200

    Second task

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
-console.log('DOM');
+const dataUrl = document.querySelectorAll('[data-url]');
+
+if(dataUrl) {
+    dataUrl.forEach(function(element) { 
+
+        let link = element.getAttribute('data-url');
+        element.setAttribute('href', link);
+    });
+}
