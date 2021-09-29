9e0ec950ca0783ef2a0c42b4b99a9765e5874c4d
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-29 15:50:13 +0200

    First task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,12 @@
-console.log('DOM');
+const comments = document.querySelector('.comments__item.comments__item--newest');
+
+console.log(comments);
+
+if (comments) {
+    const dataset = comments.querySelectorAll('[data-info]');
+
+    dataset.forEach(function(el){
+        console.log(el);
+    });
+
+}
