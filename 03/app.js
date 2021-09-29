02395345028a06ac1239339457cffa41661764b3
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-29 17:25:44 +0200

    Third task

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,3 @@
-console.log('DOM');
-
 const buttonSettings = {
     attr: {
         className: 'btn',
@@ -11,4 +9,26 @@ const buttonSettings = {
         color: '#444'
     },
     text: 'Click me!',
+}
+
+
+
+const section = document.querySelector('.parent-for-button');
+
+if (section) {
+
+    const newButton = document.createElement('button');
+
+    section.appendChild(newButton);
+
+    newButton.classList.add(buttonSettings.attr.className);
+    newButton.setAttribute('title', buttonSettings.attr.title);
+
+    console.log(buttonSettings.attr.className);
+    
+    for (const element in buttonSettings.css) {
+        newButton.style[element] = buttonSettings.css[element]
+    }
+
+    newButton.innerText=buttonSettings.text;
 }
