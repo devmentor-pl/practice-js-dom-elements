d968fe9502d678563e14e2e1acb1c3459df12a72
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-29 21:27:36 +0200

    Fourth task

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,56 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+console.log(nav);
+const newUl = document.createElement('ul');
+
+nav.appendChild(newUl);
+
+
+for (let i=0; i<3; i++) {
+
+    const newLi = document.createElement('li');
+    newUl.appendChild(newLi);
+
+    const newA = document.createElement('a');
+    newLi.appendChild(newA);
+
+    console.log(newA);
+
+    newA.innerText = menuItems[i].text;
+    newA.setAttribute('href',menuItems[i].url);
+}
+
+console.log(menuItems[0].text);
+
+
+//poniżej tylko serio nieudanych prób :). Któraś była blisko z wykorzystaniemm forEach?
+//jaki jest inny sposób na to zadanie?
+
+
+
+// let aElements = document.querySelectorAll('a');
+
+// for (i = 0; i < aElements.length; i++) {
+    
+//         aElements[i].setAttribute('href', menuItems.forEach(function(el) {
+//             return el.text;
+//         }));
+//   }
+
+
+// aElements.forEach(function(el) {
+//     console.log(el);
+// })
+
+// console.log(aElements);
+
+// menuItems.forEach(function(el) {
+//     console.log(el.text);
+// });
+
+// console.log(menuItems);
+
+
