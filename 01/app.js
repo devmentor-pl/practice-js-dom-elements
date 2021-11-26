f6d9a6fbb00a8953647c4fa3b70d6573ffa29230
Ewelina <ewelina@ekopacz.pl>
2021-11-26 13:21:42 +0100

    add solution

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+const mainElement = document.querySelector('.comments__item.comments__item--newest');
+
+if(mainElement){
+    const spanList = mainElement.querySelectorAll('p > span');
+    const dataInfoList = [];
+
+    spanList.forEach(function(element){
+        if(element.hasAttribute('data-info')){
+            dataInfoList.push(element);
+        }
+    })
+    console.log('Ilosc elementow, ktore posidaja atrybut data-info to: ' + dataInfoList.length);
+}
