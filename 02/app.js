e65268d42f240840cf137a2d85f8496f0e2472db
Ewelina <ewelina@ekopacz.pl>
2021-11-26 22:54:44 +0100

    add solution

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,19 @@
 console.log('DOM');
+
+const aList = document.querySelectorAll('a');
+
+if(aList){
+aList.forEach(function(element){
+    const array = [];
+        if(element.innerText !==null){
+            if(element.hasAttribute('data-url')){
+                array.push(element.getAttribute('data-url'));
+                console.log(array);
+            }
+            if(element){
+            element.setAttribute('href',array);
+            }
+        }
+    });
+}
+console.log(aList);
