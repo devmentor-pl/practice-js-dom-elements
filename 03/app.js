cf172c44b28d1cc28867f1cabd1db47fe861facc
Ewelina <ewelina@ekopacz.pl>
2021-11-27 12:25:47 +0100

    add part of solution

--- 03/app.js
+++ 03/app.js
@@ -6,9 +6,33 @@ const buttonSettings = {
         title: 'super button'
     },
     css: {
-        border: '1px solid #336699',
+        border: '10px solid #336699',
         padding: '5px 20px',
         color: '#444'
     },
     text: 'Click me!',
+}
+
+const buttonParent = document.querySelector('.parent-for-button');
+
+if(buttonParent){
+const button = document.createElement('button');
+buttonParent.appendChild(button);
+
+button.className = 'btn';
+button.title = "super button";
+button.style.border = '3px solid #336699';
+button.style.padding = '5px 20px';
+button.style.color = '#444';
+button.innerHTML = 'Click ME!';
+
+
+// for(const value in buttonSettings){
+//     button.style.value
+// }
+
+
+
+
+console.log(button);
 }
