c23ee88decfd94a43bd7b50d68d09ae2c4bf5b91
Ewelina <ewelina@ekopacz.pl>
2021-11-27 16:16:48 +0100

    add solution

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,34 @@
 console.log('DOM');
 
-// struktura do wykorzystania w pętli
-const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
-];
+// I sposób
+
+const navSection = document.querySelector('nav');
+const newUlSection = document.createElement('ul');
+
+const urlArray = ['/', '/gallery', '/contact'];
+const itemUrlArray = urlArray.values(); //Metoda values() zwraca nowy obiekt Array Item , który zawiera wartości dla każdego indeksu w tablicy.
+
+const textArray = ['start','galeria','kontakt'];
+const itemTextArray = textArray.values();
+
+if(navSection){
+    navSection.appendChild(newUlSection);
+
+    for(let i=0; i<3; i++){
+        const newLiSection = document.createElement('li');
+        const newASection = document.createElement('a');
+        newUlSection.appendChild(newLiSection).appendChild(newASection);
+    }
+}
+const AList = document.querySelectorAll('a');
+AList.forEach(function(element){
+    element.setAttribute('href',itemUrlArray.next().value);
+    element.innerHTML = itemTextArray.next().value;
+})
+
+console.log(AList);
+
+
+
+
+
---
+++ 04/app2.js
@@ -0,0 +1,36 @@
+// struktura do wykorzystania w pętli
+const menuItems = [
+    {text: 'start', url: '/'},
+    {text: 'galeria', url: '/gallery'},
+    {text: 'kontakt', url: '/contact'},
+];
+
+// II sposób z wykorzystaniem struktury
+const articleSection = document.querySelector('article');
+const new2UlSection = document.createElement('ul');
+
+if(articleSection){
+    articleSection.appendChild(new2UlSection);
+    let j=0;
+    while(j<3){
+        const new2LiSection = document.createElement('li');
+        const new2ASection = document.createElement('a');
+        new2UlSection.appendChild(new2LiSection).appendChild(new2ASection);
+        j++;
+    }
+}
+
+const A2List = document.querySelectorAll('a');
+const len = menuItems.length;
+console.log(len);
+
+A2List.forEach(function(value,index){
+        value.setAttribute('href', menuItems[index]['url']);
+        value.innerHTML = menuItems[index]['text'];
+});
+
+let names =['kate','jack','john']
+names.forEach(function(name,index){
+ console.log(index);
+
+});
