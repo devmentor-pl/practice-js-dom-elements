46ac0f790e95cc6c549bcce5be8acf77a040d2d8
Ewelina <ewelina@ekopacz.pl>
2021-11-28 18:58:51 +0100

    upgrade

--- 01/app.js
+++ 01/app.js
@@ -5,11 +5,12 @@ const mainElement = document.querySelector('.comments__item.comments__item--newe
 if(mainElement){
     const spanList = mainElement.querySelectorAll('p > span');
     const dataInfoList = [];
-
-    spanList.forEach(function(element){
-        if(element.hasAttribute('data-info')){
-            dataInfoList.push(element);
-        }
-    })
+        if(spanList){
+        spanList.forEach(function(element){
+            if(element.hasAttribute('data-info')){
+                dataInfoList.push(element);
+            }
+        })
+    }
     console.log('Ilosc elementow, ktore posidaja atrybut data-info to: ' + dataInfoList.length);
 }
