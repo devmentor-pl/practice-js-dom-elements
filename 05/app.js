f18b2b4043f7d2ce72e9cca6c4dd6a422578f3a8
Ewelina <ewelina@ekopacz.pl>
2021-11-28 18:11:47 +0100

    add solution

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,52 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const parentCurr = curr.parentElement;
+
+//1
+if(curr){
+    const buttonBro = document.createElement('button');
+    buttonBro.innerText = 'Usuń z koszyka';
+
+    parentCurr.appendChild(buttonBro);
+
+//2
+const childrenArray = parentCurr.children;
+const newChildArray = Array.from(childrenArray);
+
+newChildArray.forEach(function(element){
+    element.classList.add('sibilings');
+});
+}
+
+//3
+const nextArticle = parentCurr.nextElementSibling;
+if(nextArticle){
+    nextArticle.title= 'nextElementSibling';
+}
+
+//4
+const lastArticle = nextArticle.nextElementSibling;
+if(lastArticle){
+
+const newP = document.createElement('p');
+newP.innerText = 'New paragraph before button';
+
+const lastElementLastArticle = lastArticle.lastElementChild;
+
+lastArticle.insertBefore(newP,lastElementLastArticle);
+
+}
+
+//5
+const sectionElement = parentCurr.parentElement;
+if(sectionElement){
+
+const newArticle = parentCurr.cloneNode(true);
+sectionElement.insertBefore(newArticle,parentCurr);
+
+}
+
+
+
+
