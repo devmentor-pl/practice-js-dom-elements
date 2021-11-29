560b42c209522c29c3fd235898fc4baf14f8ded7
Ewelina <ewelina@ekopacz.pl>
2021-11-29 22:34:09 +0100

    upgrade

--- 04/app.js
+++ 04/app.js
@@ -11,22 +11,25 @@ const textArray = ['start','galeria','kontakt'];
 if(navSection){
     navSection.appendChild(newUlSection);
 
-    for(let i=0; i<3; i++){
+    for(let i=0; i<urlArray.length; i++){
         const newLiSection = document.createElement('li');
         const newASection = document.createElement('a');
+        newASection.innerHTML = textArray[i];
+        newASection.setAttribute('href',urlArray[i]);
         newUlSection.appendChild(newLiSection).appendChild(newASection);
     }
 }
 
-const AList = document.querySelectorAll('a');
-AList.forEach(function(element,index){
-    element.setAttribute('href',urlArray[index]);
-    element.innerHTML = textArray[index];
-});
-
-console.log(AList);
 
 
+
+// const AList = document.querySelectorAll('a');
+// AList.forEach(function(element,index){
+//     element.setAttribute('href',urlArray[index]);
+//     element.innerHTML = textArray[index];
+// });
+
+// console.log(AList);
 
 //Komentarz dla mnie:
 //Istnieje take metoda values() zwraca nowy obiekt Array Item , który zawiera wartości dla każdego indeksu w tablicy.
