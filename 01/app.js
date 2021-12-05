383f1609c53fc37d835bb5d62f4303efc9d943cd
Magdalena Nolde <magdalena@nolde.de>
2021-12-05 16:51:34 +0100

    first two tasks

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+const commentClass = document.getElementsByClassName('comments__item comments__item--newest');
+console.log(commentClass);
+for(let i=0; i<commentClass.length; i++) {
+const dataAttribute = commentClass[i].querySelectorAll('[data-info]');
+console.log(dataAttribute);
+const arr = Array.from(dataAttribute);
+const arrLength = console.log(arr.length);}
--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+const allLinks  = document.querySelectorAll('[data-url]');
+// console.log(allLinks);
+allLinks.forEach(function(element) { 
+    const setLink = element.getAttribute('data-url');
+    console.log(setLink);
+    element.setAttribute('href', setLink);
+    });
