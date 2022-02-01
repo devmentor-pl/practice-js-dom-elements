297b57a56c489195b35c94d77d9aab20e38ebb86
Max <maciejdobisz@gmail.com>
2022-02-01 09:54:57 +0000

    practice js dom elements

--- 02/app.js
+++ 02/app.js
@@ -2,9 +2,9 @@ console.log('DOM');
 
 const links = document.querySelectorAll('[data-url]');
 
-if(links !== null) {
+if(links.length !== 0) {
     links.forEach(function(item) {
-        let x = item.getAttribute('data-url');
-        item.setAttribute('href',x);
+        let itemAttribute = item.getAttribute('data-url');
+        item.setAttribute('href',itemAttribute);
     })
 }
--- 05/app.js
+++ 05/app.js
@@ -9,30 +9,25 @@ if(curr !== null) {
     newButton.innerText = 'usun z koszyka';
     currParent.insertBefore(newButton, curr);
 
-
     const currParentChildsList = currParent.children;
     const parentChildsArr = Array.from(currParentChildsList);
-    for(let i=0; i<parentChildsArr.length-1; i++) {
-        parentChildsArr[i].classList.add('siblings');
-    }
+    parentChildsArr.forEach(function(element) {
+        if(element.className !== 'article__btn js-curr') {
+            element.classList.add('siblings')
+        }
+    })
 
-
-    const secondArticle = currParent.nextSibling.nextSibling;
+    const secondArticle = currParent.nextElementSibling;
     secondArticle.setAttribute('title','nextElementSibling');
 
-
-    const thirdArticle = secondArticle.nextSibling.nextSibling;
+    const thirdArticle = secondArticle.nextElementSibling;
     const thirdArticleChildsList = thirdArticle.children;
     const thirdArticleChildsListARR = [...thirdArticleChildsList];
     const lastChild = thirdArticleChildsListARR[2];
     const par = document.createElement('p');
     thirdArticle.insertBefore(par, lastChild);
 
-
     const section = curr.parentElement.parentElement;
     const articleClone = currParent.cloneNode(true);
     section.insertBefore(articleClone, currParent);
 }
-
-
-
