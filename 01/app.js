a99d1a95a8a47451b7b2de8a28cc1cbb775073b6
Tomasz Cividino <tomaszcividino@gmail.com>
2022-02-09 13:26:27 +0100

    DOM ELEMENTS

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,13 @@
-console.log('DOM');
+const docs = document.getElementsByClassName('comments__item comments__item--newest')
+
+const docsArray = Array.from(docs)
+
+docsArray.forEach(function(item) {
+   const spanItems = item.getElementsByTagName('span')
+   const spanArray = Array.from(spanItems)
+   spanArray.forEach(function(item) {
+      console.log(item.hasAttribute('data-info'), item.dataset.info)
+    })
+})
+
+
--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,48 @@
-console.log('DOM');
 
-// struktura do wykorzystania w pętli
+const nav = document.querySelector('nav')
+
+const ulHTML = document.createElement('ul')
+
+let newUl = nav.appendChild(ulHTML)
+
+let ulLength = newUl.children.length
+
+let newLi
+let arrLi = [];
+
+while (ulLength < 3) {
+  ulLength++;
+  newLi = document.createElement('li')
+  newUl.appendChild(newLi)
+  arrLi.push(newLi)
+}
+
+arrLi[0].innerHTML = '<a href="/">start</a>'
+arrLi[1].innerHTML = '<a href="/gallery">galeria</a>'
+arrLi[2].innerHTML = '<a href="/contact">kontakt</a>'
+
+
 const menuItems = [
     {text: 'start', url: '/'},
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+menuItems.forEach(function(objects) {
+  for(keys in objects) {
+    const values = objects[keys]
+    if (values === 'start') 
+      arrLi[0].innerHTML = '<a href=/>' + values + '</a>'
+    if (values === 'galeria') 
+      arrLi[1].innerHTML = '<a href=/gallery>' + values + '</a>'
+    if (values === 'kontakt') 
+      arrLi[2].innerHTML = '<a href=/contact>' + values + '</a>'
+    }  
+  })
+  
+  console.log(document.body)
+
+
+
+
+
