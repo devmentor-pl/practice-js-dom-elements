881d330a78853ba43b517fd44e7f2512efd52be9
Anna <krolikowska.ann@gmail.com>
2022-02-26 19:23:32 +0100

    02 added setAttribute

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('[data-url]');
+console.log(links);
+
+links.forEach(function(element) {
+    const link = element.getAttribute('data-url');
+    element.setAttribute('href', link)
+});
