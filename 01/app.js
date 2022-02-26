a0aaa60fa26a6857e76cd090c4f3e782b5ec5e98
Anna <krolikowska.ann@gmail.com>
2022-02-26 18:50:20 +0100

    01 added query selectors

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const comment = document.querySelector('.comments__item.comments__item--newest');
+console.log(comment);
+if(comment) {
+   const dataset = comment.querySelectorAll('[data-info]')
+    console.log('Liczba elementów z atrybutem data-info: ' + dataset.length);
+}
