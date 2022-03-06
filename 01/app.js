93934176e4c244ae6244327c289fc4f71a05321b
Jakub Parol <kubciop33@gmail.com>
2022-03-06 13:55:00 +0100

    DOM elements

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+
+if(element) {
+    const dataInfoList = element.querySelectorAll('[data-info]')
+    console.log(dataInfoList.length);
+}
