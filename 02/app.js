8194c824fe216ea7891a505ae67523f7dafc4525
Jakub Parol <kubciop33@gmail.com>
2022-03-06 13:53:52 +0100

    DOM elements

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('[data-url]');
+
+links.forEach(function(element) {
+    const link = element.getAttribute('data-url');
+    element.setAttribute('href', link);
+})
--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,47 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const navElement = document.querySelector('nav');
+
+if(navElement) {
+    const newMenu = document.createElement('ul');
+    navElement.appendChild(newMenu);
+
+    menuItems.forEach(function(element) {
+        const newItem = document.createElement('li');
+        newMenu.appendChild(newItem);
+        newItem.innerHTML = '<a href="'+ element.url + '">' + element.text + '</a>';
+    })
+}
+
+
+// const navElement = document.querySelector('nav');
+
+// if(navElement) {
+//     const newMenu = document.createElement('ul');
+//     navElement.appendChild(newMenu);
+//     if(newMenu) {
+//         const newItem = document.createElement('li');
+//         const cloneItem = newItem.cloneNode();
+//         const cloneItem2 = newItem.cloneNode();
+//         newMenu.appendChild(newItem);
+//         newMenu.appendChild(cloneItem);
+//         newMenu.appendChild(cloneItem2);
+
+//             const newLink = document.createElement('a');
+//             newLink.innerText = 'start';
+//             newLink.setAttribute('href', '/');
+//             newItem.appendChild(newLink);
+
+//             const cloneLink = document.createElement('a');
+//             cloneLink.innerText = 'galeria';
+//             cloneLink.setAttribute('href', '/gallery');
+//             cloneItem.appendChild(cloneLink);
+
+//             const cloneLink2 = document.createElement('a');
+//             cloneLink2.innerText = 'contact';
+//             cloneLink2.setAttribute('href', '/contact');
+//             cloneItem2.appendChild(cloneLink2);
+//     }
+// }
