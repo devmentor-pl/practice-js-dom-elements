be9822e9328729f2286c48e097961f50f57212af
Lukasz Konatowski <konat1993@gmail.com>
2022-03-14 18:43:04 +0100

    task #2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
-console.log('DOM');
+console.log('DOM')
+
+const anchorsWithDataUrl = document.querySelectorAll("[data-url]")
+if (anchorsWithDataUrl) {
+    anchorsWithDataUrl.forEach(anchor => {
+        const getDataUrl = anchor.getAttribute("data-url")
+        anchor.setAttribute("href", getDataUrl)
+    })
+}
