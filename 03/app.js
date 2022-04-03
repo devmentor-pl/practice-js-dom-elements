b1cf82a1c9063220d0b11036d5c1812d489b85c7
Filip <43039909+Urfisior@users.noreply.github.com>
2022-04-03 23:38:23 +0100

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,22 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+const parentForBtn = document.querySelector('.parent-for-button');
+if (parentForBtn) {
+    const btn = document.createElement('button');
+
+    for (const value in buttonSettings) {
+        if (value === 'text') btn.innerText = `${buttonSettings[value]}`
+        const keys = buttonSettings[value];
+        for (const value in keys) {
+            if (value === 'className') btn.classList.add(keys[value]);
+            if (value === 'title') btn.setAttribute('title',keys[value]);
+            if (value === 'border') btn.style.setProperty(value,keys[value]);
+            if (value === 'padding') btn.style.setProperty(value, keys[value]);
+            if (value === 'color') btn.style.setProperty(value, keys[value]);
+        }
+
+    }
+    parentForBtn.appendChild(btn)
+    console.log(btn);
+}
