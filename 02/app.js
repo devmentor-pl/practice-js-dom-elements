cb2921a52df0b4f6441eca939b169de2076bc1c5
Filip <43039909+Urfisior@users.noreply.github.com>
2022-04-03 22:39:35 +0100

    task 02 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+const links = document.querySelectorAll('[data-url]');
+if (links) {
+    links.forEach(function (el) {
+        const dataAttr = el.dataset.url;
+        el.setAttribute('href', dataAttr);
+    });
+};
