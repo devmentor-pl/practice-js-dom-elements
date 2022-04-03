8b34374ca858a7a8385d5cf5619c2a1d55926c91
Filip <43039909+Urfisior@users.noreply.github.com>
2022-04-03 22:24:20 +0100

    task 01 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,5 @@
 console.log('DOM');
+const liElement = document.querySelector('.comments__item, .comments__item--newest');
+const spanElements = liElement.querySelectorAll('[data-info]');
+console.log(spanElements);
+
