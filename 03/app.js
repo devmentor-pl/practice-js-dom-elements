e88941192c85d62d17f5d0e93adf2e716cc83668
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-04-07 11:59:42 +0200

    task 3

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,22 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+
+const parentElement = document.querySelector('.parent-for-button')
+
+if(parentElement){
+    const newButton = document.createElement('button')
+
+    for(const key in buttonSettings.attr){
+    newButton.setAttribute(key, buttonSettings.attr[key])
+}
+    for(const key in buttonSettings.css){
+    newButton.style[key] = buttonSettings.css[key]
+}
+
+    newButton.innerText = 'Click me!'
+
+    parentElement.appendChild(newButton)
+}
+
