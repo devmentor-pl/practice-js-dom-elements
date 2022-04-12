171d154cb89358be4e8c6692097819dca15e50ed
Andrzej <andis_as@wp.pl>
2022-04-12 21:57:03 +0200

    zad 3 solution Two by for in

--- 03/app.js
+++ 03/app.js
@@ -15,32 +15,49 @@ const buttonSettings = {
 
 // solution One
 // ==============================
-console.log('---- solution One ----')
-const button = document.createElement('button')
+// console.log('---- solution One ----')
+// const button = document.createElement('button')
 
-// attr
-button.className = buttonSettings.attr.className
-button.title = buttonSettings.attr.title
+// // attr
+// button.className = buttonSettings.attr.className
+// button.title = buttonSettings.attr.title
 
-// css
-objCss = buttonSettings.css
-for(let key in objCss) {
-    let css = objCss[key]
-    button.style[key] = css
-}
+// // css
+// objCss = buttonSettings.css
+// for(let key in objCss) {
+//     let css = objCss[key]
+//     button.style[key] = css
+// }
 
-// text
-button.innerText = buttonSettings.text
+// // text
+// button.innerText = buttonSettings.text
 
-// add to browser
-const section = document.querySelector('.parent-for-button')
-section.appendChild(button)
+// // add to browser
+// const section = document.querySelector('.parent-for-button')
+// section.appendChild(button)
 
 
 // solution Two
 // ==============================
-// console.log('---- solution Two ----')
+console.log('---- solution Two ----')
+const button = document.createElement('button')
+for(let key in buttonSettings) {
+    // console.log(key)
+    // console.log(buttonSettings[key])
+    if(key === 'attr') {
+        button.className = buttonSettings[key].className
+        button.title = buttonSettings[key].title
+    }
+    if(key === 'css') {
+        button.style.border = buttonSettings[key].border
+        button.style.padding = buttonSettings[key].padding
+        button.style.color = buttonSettings[key].color
+    }
+    button.innerText = buttonSettings[key]
+}
 
+const section = document.querySelector('.parent-for-button')
+section.appendChild(button)
 
 
 
