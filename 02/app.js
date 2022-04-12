8fbfe839ffe9eef501cd0d67150d63b22e4e0c86
Andrzej <andis_as@wp.pl>
2022-04-12 20:58:02 +0200

    zad 2 preparing

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,30 @@
-console.log('DOM');
+console.log('ZAD-2')
+
+const links = document.querySelectorAll('ul > li > a')
+console.log(links)
+
+const arr = []
+links.forEach(function(item) {
+    let data = item.getAttribute('data-url')
+    if(data) {
+        arr.push(data)
+        console.log(item)
+        item.setAttribute('href', data)
+    }
+})
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
