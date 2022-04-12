94eae8304adfc71b08128524d3e6c035e8d6e26e
Mateusz Suplewski <matx3582@gmail.com>
2022-04-12 19:01:10 +0200

    Task-1 Done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const classifiedElement = document.querySelector(".comments__item.comments__item--newest");
+
+if (classifiedElement) {
+    const elementWithAttribute = classifiedElement.querySelectorAll('[data-info]');
+    console.log(elementWithAttribute.length);
+}
+
