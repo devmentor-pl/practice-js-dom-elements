97172d6826c75977573d9e3202f98c99031431aa
Andrzej <andis_as@wp.pl>
2022-04-12 21:54:49 +0200

    zad 3 solution One

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,4 @@
-console.log('DOM');
+console.log('ZAD-3');
 
 const buttonSettings = {
     attr: {
@@ -12,3 +12,48 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+// solution One
+// ==============================
+console.log('---- solution One ----')
+const button = document.createElement('button')
+
+// attr
+button.className = buttonSettings.attr.className
+button.title = buttonSettings.attr.title
+
+// css
+objCss = buttonSettings.css
+for(let key in objCss) {
+    let css = objCss[key]
+    button.style[key] = css
+}
+
+// text
+button.innerText = buttonSettings.text
+
+// add to browser
+const section = document.querySelector('.parent-for-button')
+section.appendChild(button)
+
+
+// solution Two
+// ==============================
+// console.log('---- solution Two ----')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
