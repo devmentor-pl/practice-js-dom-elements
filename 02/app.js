1c8f2a0c2e00d3a85c7688a0c93896841dfb3a55
Andrzej <andis_as@wp.pl>
2022-04-12 20:59:37 +0200

    zad 2 done

--- 02/app.js
+++ 02/app.js
@@ -3,16 +3,13 @@ console.log('ZAD-2')
 const links = document.querySelectorAll('ul > li > a')
 console.log(links)
 
-const arr = []
 links.forEach(function(item) {
     let data = item.getAttribute('data-url')
     if(data) {
-        arr.push(data)
         console.log(item)
         item.setAttribute('href', data)
     }
 })
-console.log(arr)
 
 
 
