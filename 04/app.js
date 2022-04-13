bffea9258fa90cc491f81e12c47317bd085c18ac
Andrzej <andis_as@wp.pl>
2022-04-13 10:23:57 +0200

    zad 4 done

--- 04/app.js
+++ 04/app.js
@@ -1,4 +1,4 @@
-console.log('DOM');
+console.log('ZAD 4');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
@@ -6,3 +6,22 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+
+const ul = document.createElement('ul')
+
+menuItems.forEach(item => {
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    a.setAttribute('href', item.url)
+    a.textContent = item.text
+    li.appendChild(a)
+    ul.appendChild(li)
+})
+
+const nav = document.querySelector('nav')
+nav.appendChild(ul)
+
+
+
+
