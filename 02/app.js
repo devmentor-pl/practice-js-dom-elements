06453d2ab81d66d194d28b14dbad0477f79fd484
Camila Montes Celinski <camicelinski@gmail.com>
2022-04-13 16:48:53 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const aList = document.querySelectorAll('a[data-url]');
+
+aList.forEach(function(el) {
+    for(let i=0; i < aList.length; i++) {
+        let urlEl = aList[i].dataset.url
+        aList[i].href = urlEl 
+    }
+})
