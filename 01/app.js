05c57f865fe0f16c5cde79c3d8ab9a374dd3b787
Lukasz Scisel <lukscidev@gmail.com>
2022-04-14 14:26:00 +0200

    another way of define classes

--- 01/app.js
+++ 01/app.js
@@ -1,6 +1,6 @@
 console.log('DOM');
 
-const commentsContainer = document.querySelector('.comments__item, .comments__item--newest')
+const commentsContainer = document.querySelector('.comments__item.comments__item--newest')
 console.log(commentsContainer)
 
 if (commentsContainer) {
