b633fec89d1d272fb09f9fd3c99b37992a2c4841
Camila Montes Celinski <camicelinski@gmail.com>
2022-04-14 02:51:12 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,19 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parentForButton = document.querySelector('.parent-for-button')
+const newButton = document.createElement('button')
+parentForButton.appendChild(newButton)
+
+const attributes = buttonSettings.attr
+for(let attr in attributes) {
+    newButton.setAttribute(attr, attributes[attr])
+}
+
+const styles = buttonSettings.css
+for(let property in styles) {
+    newButton.style.setProperty(property, styles[property])
+}
+
+newButton.innerText = buttonSettings.text
