a0bfd1f71fda9f517a5b341f383d1f2e6e35e245
Martyna Giee <martyna.giee@gmail.com>
2022-04-16 13:49:47 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const allLinks = document.querySelectorAll("a[data-url]");
+
+[...allLinks].forEach(link => {
+
+    let dataUrlAtt = link.getAttribute("data-url")
+    link.setAttribute("href", dataUrlAtt)
+})
