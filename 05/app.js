65a7f522fe7dd78b0172ac7ce98427275abfeb5f
Martyna Giee <martyna.giee@gmail.com>
2022-04-17 23:32:50 +0200

    add main function in task 5

--- 05/app.js
+++ 05/app.js
@@ -1,32 +1,34 @@
-console.log('DOM');
+(function () {
+    console.log('DOM');
 
-const curr = document.querySelector('.js-curr')
+    const curr = document.querySelector('.js-curr')
 
 
-//dodanie buttona
-const deleteBtn = document.createElement('button')
-deleteBtn.innerText = "Usuń z koszyka"
-curr.parentElement.appendChild(deleteBtn)
+    //dodanie buttona
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "Usuń z koszyka"
+    curr.parentElement.appendChild(deleteBtn)
 
-// dodanie klasy siblings
-let allChildrens = [...curr.parentElement.children]
-allChildrens.forEach(child => child.classList.add("sibling"))
+    // dodanie klasy siblings
+    let allChildrens = [...curr.parentElement.children]
+    allChildrens.forEach(child => child.classList.add("sibling"))
 
-// dodanie title do kolejnego .article
-curr.parentElement.nextElementSibling.setAttribute("title", "nextElementSibling")
+    // dodanie title do kolejnego .article
+    curr.parentElement.nextElementSibling.setAttribute("title", "nextElementSibling")
 
-// Dodanie paragrafu
-const newParagraph = document.createElement('p')
-newParagraph.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, asperiores.';
+    // Dodanie paragrafu
+    const newParagraph = document.createElement('p')
+    newParagraph.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, asperiores.';
 
-const allArticles = [...curr.parentElement.parentElement.children]
-const lastArticle = allArticles[allArticles.length - 1];
-const lastArticleChildren = [...allArticles[allArticles.length - 1].children];
-const articleElementsNumber = lastArticleChildren.length
-lastArticle.insertBefore(newParagraph, lastArticleChildren[lastArticleChildren.length - 1])
+    const allArticles = [...curr.parentElement.parentElement.children]
+    const lastArticle = allArticles[allArticles.length - 1];
+    const lastArticleChildren = [...allArticles[allArticles.length - 1].children];
+    const articleElementsNumber = lastArticleChildren.length
+    lastArticle.insertBefore(newParagraph, lastArticleChildren[lastArticleChildren.length - 1])
 
-//dodanie kolejnego article
-const newArticle = curr.parentElement.cloneNode(true)
-newArticle.children[0].innerText = "New article"
-curr.parentElement.parentElement.appendChild(newArticle)
-
+    //dodanie kolejnego article
+    const newArticle = curr.parentElement.cloneNode(true)
+    newArticle.children[0].innerText = "New article"
+    curr.parentElement.parentElement.appendChild(newArticle)
+}
+)()
