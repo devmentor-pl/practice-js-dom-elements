ae1e40ce7e49fb630ddcf137eec13aa5af484469
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-04-21 10:33:39 +0200

    Zadanie nr 1 wykonane

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,3 @@
-console.log('DOM');
+const element = document.querySelector('.comments__item.comments__item--newest')
+const dataInfoList = element.querySelectorAll('[data-info]')
+console.log('Znaleziono ' + dataInfoList.length + ' takie elementy');
