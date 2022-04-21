45afc97f011d71e09ab36dc67f963e93d306f566
Dawid Drag <dede7d@wp.pl>
2022-04-21 18:48:05 +0200

    added js code

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,16 @@
 console.log('DOM');
+
+const twoClassElement = document.querySelector('.comments__item.comments__item--newest');
+
+console.log(twoClassElement);
+
+if (twoClassElement) {
+    const elementWithAttribute = twoClassElement.querySelectorAll('[data-info]');
+
+    console.log(elementWithAttribute);
+}
+
+
+
+
+
