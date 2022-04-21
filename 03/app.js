1045723d39d25f44eb7e2c157bba3e188950c6c7
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-04-21 11:46:36 +0200

    Zadanie nr 3 wykonane

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,34 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const buttonParent = document.querySelector('.parent-for-button');
+
+if(buttonParent) {
+    const btnEl = document.createElement('button');
+    buttonParent.appendChild(btnEl);
+    for(keys in buttonSettings) {
+        const value = buttonSettings[keys];
+        if(keys === 'text') {
+            btnEl.textContent = value;
+        }
+        for(keys in value) {
+            if(keys === 'className') {
+                btnEl.classList.add(value[keys]);
+            }
+            else if(keys === 'title') {
+                btnEl.setAttribute('title', value[keys]);
+            }
+            else if(keys === 'border') {
+                btnEl.style.border = value[keys];
+            }
+            else if(keys === 'padding') {
+                btnEl.style.padding = value[keys];
+            }
+            else if(keys === 'color') {
+                btnEl.style.color = value[keys];
+            }
+        }
+
+    }
+}
