a8016775ca27b75977717d742b91acca451c7f9b
Dawid Drag <dede7d@wp.pl>
2022-04-23 21:14:52 +0200

    added two task

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,18 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newButton = document.createElement('button');
+
+newButton.innerText = 'Usuń z koszyka.';
+
+curr.parentElement.appendChild(newButton);
+
+const childrenFromParentElement = curr.parentElement.children;
+
+console.log(childrenFromParentElement);
+
+for (let i = 0; i < childrenFromParentElement.length; i++) {
+    console.log(i);
+    childrenFromParentElement[i].classList.add('siblings');
+}
