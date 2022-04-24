095fe166797485257fed4a81c5f9f6a2107cc640
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2022-04-24 15:52:44 +0200

    tasks completed

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,12 @@
 console.log('DOM');
+
+const newestComment = document.getElementsByClassName('comments__item', 'comments__item--newest')
+console.log(newestComment)
+if (newestComment) {
+    for (let i = 0; i < newestComment.length; i++) {
+        const newestCommentInfo = newestComment[i].querySelectorAll('[data-info]')
+        console.log(newestCommentInfo)
+        console.log('Znaleziono ' + newestCommentInfo.length + ' elementy/ów')
+    }
+}
+
--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,26 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+const parentForButton = document.querySelector('.parent-for-button')
+const button = document.createElement('button')
+parentForButton.appendChild(button)
+
+
+// ręcznie
+// button.style.border = "1px solid #336699"
+// button.style.color = "#444"
+// button.style.padding = "5px 20px"
+
+
+for (const key in buttonSettings.attr) {
+    const value = buttonSettings.attr[key]
+    button[key] = value
+}
+
+for (const key in buttonSettings.css) {
+    const value = buttonSettings.css[key]
+    button.style[key] = value
+}
+
+button.textContent = buttonSettings.text
+
