25326d7bf216ea98e1cb8f851e9c4c44c564ad98
justyna <strawberka17@gmail.com>
2022-04-27 12:52:11 +0100

    finished task3

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,31 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const button=document.createElement('button')
+const parents=document.querySelector('.parent-for-button')
+parents.appendChild(button)
+
+button.innerText=buttonSettings.text
+for(const key in buttonSettings.attr){
+    const value=buttonSettings.attr[key]
+    console.log(value)
+    button[key]=value
+    console.log(button)
+}
+
+for(const key in buttonSettings.css){
+    const styles=buttonSettings.css[key]
+    button.style[key]=styles 
+}
+
+
+
+
+
+
+
+
+for(const property in buttonSettings){
+    console.log(`${property} : ${buttonSettings[property]}`)
+}
