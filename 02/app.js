bb886a37114d25ad4441bad8f28fbc7da2c3199c
justyna <strawberka17@gmail.com>
2022-04-27 09:06:00 +0100

    finished task2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+const element=document.querySelectorAll('[data-url]')
+
+element.forEach(function(e){
+    const link=e.getAttribute('data-url')
+    e.setAttribute('href',link)
+})
+
