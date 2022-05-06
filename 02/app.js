8794a3b7f3249bb1f4d623c71fdfa19f8f8bbba0
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-05-06 12:16:15 +0200

    zad2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const dataUrl = document.querySelectorAll('a')
+
+for(let i = 0; i < dataUrl.length; i++){
+    if(dataUrl[i].hasAttribute('data-url')){
+        let urlAdres = dataUrl[i].dataset.url;
+        dataUrl[i].setAttribute('href', urlAdres)
+    }
+}
