7d67ff2b34df8719876391cb93400dfeb887e164
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-05-06 12:04:39 +0200

    zad1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,20 @@
 console.log('DOM');
+
+let dataInfoCounter = 0;
+const commentsItemElements = document.querySelector('.comments__item, .comments__item--newest');
+
+
+const dataInfoElements = commentsItemElements.querySelectorAll('span');
+
+for(let i = 0; i < dataInfoElements.length; i++){
+    if(dataInfoElements[i].hasAttribute('data-info')){
+        dataInfoCounter += 1;
+    }
+}
+
+console.log('Ilość elementów ze znacznikiem "data-info" wynosi:  ' + dataInfoCounter);
+
+
+
+
+
