b61df208fcd66d261a314d9c27dddb45358d0311
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-05-07 22:22:11 +0200

    zad3 with for..in loop

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,29 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const button = document.createElement('button');
+console.log(button.innerHTML)
+
+for (const group in buttonSettings){
+    if(typeof buttonSettings[group] === 'object'){
+        for (const value in buttonSettings[group]){
+            // console.log(group)
+            // console.log(value)
+            // console.log(buttonSettings[group][value])
+            if(group === 'attr'){
+                button.setAttribute(value, buttonSettings[group][value])
+            }else if(group ==='css'){
+                button.style[value] = buttonSettings[group][value]
+            }
+        }
+    } else {
+        button.innerText = buttonSettings[group];
+    } 
+}
+
+
+const sectionButton = document.querySelector('.parent-for-button')
+if(sectionButton){
+    sectionButton.appendChild(button)
+}
