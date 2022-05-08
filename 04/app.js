a2d49c2e1e5042856dcf7bd714c32891fcdbfb05
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-05-08 17:57:01 +0200

    zad 4 with forEach function and .innerHTML

--- 04/app.js
+++ 04/app.js
@@ -7,9 +7,31 @@ const menuItems = [
     {text: 'kontakt', url: '/contact'},
 ];
 
+//part1
+
+// const menu = document.querySelector('nav')
+// menu.innerHTML=`<ul>
+// <li><a href="/">start</a></li>
+// <li><a href="/gallery">galeria</a></li>
+// <li><a href="/contact">kontakt</a></li>
+// </ul>`
+
+// end of part1
+
+// part2
+
+let liCodeAll = ''
+let menuCodeAll = ''
+const liMenu = menuItems.forEach(function(object) {
+    let textMenu = object.text
+    let linkMenu = object.url
+    let liCodeOneLine = '<li><a href="' + linkMenu + '">' + textMenu + '</a></li>'
+    liCodeAll += liCodeOneLine
+})
+
+menuCodeAll = '<ul>' + liCodeAll + '<ul>'
+
 const menu = document.querySelector('nav')
-menu.innerHTML=`<ul>
-<li><a href="/">start</a></li>
-<li><a href="/gallery">galeria</a></li>
-<li><a href="/contact">kontakt</a></li>
-</ul>`
+menu.innerHTML = menuCodeAll
+
+//end of part2
