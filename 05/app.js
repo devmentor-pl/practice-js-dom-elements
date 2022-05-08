589499228eee2f9e6b272809cb7512696842cf71
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-05-08 18:35:49 +0200

    zad5 p1

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,10 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+//1
+const button2 = document.createElement('button')
+button2.innerText = 'Usuń z koszyka!'
+curr.parentElement.appendChild(button2)
+
+//2
