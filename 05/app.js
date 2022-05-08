e06c008b519c2db75b67661a3da3bb0a827a1eea
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-05-08 21:36:41 +0200

    zad5 p3

--- 05/app.js
+++ 05/app.js
@@ -1,11 +1,12 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const currParent = curr.parentElement;
 
 //1
 const button2 = document.createElement('button')
 button2.innerText = 'Usuń z koszyka!'
-curr.parentElement.appendChild(button2)
+currParent.appendChild(button2)
 // const button3 = document.createElement('button')
 // button3.innerText = 'Usuń z koszyka2!'
 // curr.parentElement.appendChild(button3)
@@ -19,7 +20,12 @@ let siblings = curr.nextElementSibling
 while(siblings){
     siblings.setAttribute('class','siblings')
     siblings = siblings.nextElementSibling
-    console.log(siblings)
 }
 
 //3
+
+const siblingsArticle = currParent.nextElementSibling
+
+if(siblingsArticle.hasAttribute('class') && siblingsArticle.getAttribute('class').includes('article')){
+    siblingsArticle.setAttribute('title', 'nextElementSibling')
+} 
