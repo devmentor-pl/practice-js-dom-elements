7528846fccc4dd9d99368bf66d793ef655817327
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-09 18:29:25 +0200

    Finish task 04

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,22 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+const ulElements = document.createElement('ul');
+if (nav) {
+    nav.appendChild(ulElements);
+    for (let i = 0; i < menuItems.length; i++) {
+        let liElement = document.createElement('li');
+        let aElement = document.createElement('a');
+        ulElements.appendChild(liElement);
+        liElement.appendChild(aElement);
+    }
+}
+
+const aElementList = document.querySelectorAll('a');
+
+aElementList.forEach(function (element, index) {
+    element.setAttribute('href', menuItems[index]['url']);
+    element.textContent = menuItems[index]['text'];
+})
