e7feb98cce7eb66fa3b1a08e1490eb6918d7ca8a
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-09 17:06:31 +0200

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const comments = document.querySelector('.comments__item.comments__item--newest');
+
+if (comments) {
+	const commentsItems = comments.querySelectorAll('[data-info]');
+	console.log(`Zostały znalezione ${commentsItems.length} elementy.`);
+}
