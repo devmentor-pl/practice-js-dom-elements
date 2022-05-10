c4edded90835ea18d34bc71f64919892baabd0df
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-10 17:49:56 +0200

    fix 03 task

--- 03/app.js
+++ 03/app.js
@@ -1,30 +1,29 @@
 console.log('DOM');
 
 const btn = document.createElement('button');
-const parrentForBtn = document.querySelector('.parent-for-button');
-parrentForBtn.appendChild(btn);
-
+const parentForBtn = document.querySelector('.parent-for-button');
+parentForBtn.appendChild(btn);
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
-}
+	attr: {
+		className: 'btn',
+		title: 'super button',
+	},
+	css: {
+		border: '1px solid #336699',
+		padding: '5px 20px',
+		color: '#444',
+	},
+	text: 'Click me!',
+};
 
 if (btn) {
-    for (const key in buttonSettings.attr) {
-        btn.setAttribute(key, buttonSettings.attr[key]);
-    }
-    for (const key in buttonSettings.css) {
-        btn.setAttribute(key, buttonSettings.css[key]);
-    }
+	for (const key in buttonSettings.attr) {
+		btn.setAttribute(key, buttonSettings.attr[key]);
+	}
+	for (const key in buttonSettings.css) {
+		const value = buttonSettings.css[key];
+		btn.style[key] = value;
+	}
+	btn.innerHTML = buttonSettings.text;
 }
-
-btn.innerHTML = buttonSettings.text;
