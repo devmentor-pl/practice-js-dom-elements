9085f3f5fce9c5bc537e676aea59305d83a697eb
Angelika Rybicka <rybicka.angelika@gmail.com>
2022-06-03 13:54:14 +0200

    finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('[data-url]')
+console.log(links)
+
+links.forEach(function (element) {
+    const attr = element.getAttribute('data-url')
+
+    element.setAttribute('href', attr)
+})
