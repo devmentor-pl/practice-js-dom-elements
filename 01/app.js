6a9753c2a9727f283c2b773f13b61266e7bb8035
Marta Stelmach <marta.ms1961@gmail.com>
2022-06-12 12:55:00 +0200

    Finish task-1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+if (element) {
+    const elementWithAttribute = element.querySelectorAll('[data-info]');
+    console.log(elementWithAttribute.length);
+};
