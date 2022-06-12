a161a9e141c1ba8f69df99b51cd7b2e84d8d9767
Marta Stelmach <marta.ms1961@gmail.com>
2022-06-12 14:01:14 +0200

    Finish task-3

--- 03/app.js
+++ 03/app.js
@@ -11,4 +11,27 @@ const buttonSettings = {
         color: '#444'
     },
     text: 'Click me!',
-}
+};
+
+const renderButton = function () {
+    const button = document.createElement('button');
+    for (const property in buttonSettings.attr) {
+        const value = buttonSettings.attr[property];
+        button[property] = value;
+    };
+    for (const property in buttonSettings.css) {
+        const value = buttonSettings.css[property];
+        button.style[property] = value;
+    };
+    button.innerText = buttonSettings.text;
+    return button;
+};
+
+const render = function () {
+    const container = document.querySelector('.parent-for-button');
+    const btn = renderButton();
+
+    container.appendChild(btn);
+};
+
+render();
