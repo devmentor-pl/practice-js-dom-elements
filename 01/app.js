8d40950d5bc0e5647ab3189be97b65a92b61bb67
Patryk Kropop <patryk.kropop@gmail.com>
2022-06-16 12:44:56 +0200

    Finished task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,19 @@
 console.log('DOM');
+
+// const sectionElement = document.querySelector('.comments__item');
+// const sectionElementTwo = document.querySelector('.comments__item--newest');
+
+// if(sectionElement && sectionElementTwo){
+//     const spanElement = sectionElementTwo.querySelectorAll('span');
+//     const spanList = spanElement.length
+//     console.log('Ilość elementów data-info to: ' + spanList)
+    
+// }
+
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+
+if(element){
+    const dataInfo = element.querySelectorAll('[data-info]');
+    console.log('Ilość elementów to: ' + dataInfo.length)
+}
