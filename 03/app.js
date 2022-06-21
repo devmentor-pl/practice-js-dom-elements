1036abdf6ba5d3cd39e643343300a9011996e526
Karolina Grzywa <grzywakarolina@gmail.com>
2022-06-22 00:38:25 +0200

    Add task 3

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,18 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const buttonParent = document.querySelector('.parent-for-button');
+const newButton = document.createElement('button');
+buttonParent.appendChild(newButton);
+
+const buttonAttributes = buttonSettings.attr;
+const styles = buttonSettings.css;
+
+for (let attr in buttonAttributes) {
+    newButton.setAttribute(attr, buttonAttributes[attr]);
+}
+for (let style in styles) {
+    newButton.style.setProperty(style, styles[style]);
+    newButton.textContent = buttonSettings.text;
+}
