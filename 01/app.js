fbd252ff6f954b191495c93495268cb637b97aed
Karolina Grzywa <grzywakarolina@gmail.com>
2022-06-22 00:15:18 +0200

    Add task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const commentsElement = document.querySelector('.comments__item.comments__item--newest');
+
+if (commentsElement && commentsElement.hasChildNodes()) {
+    const dataInfoList = commentsElement.querySelectorAll('[data-info]')
+    console.log(dataInfoList.length)
+}
