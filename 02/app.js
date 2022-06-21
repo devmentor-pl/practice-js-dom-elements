1ebff22bb8d30f61686e74cff8acef89de5360e4
Karolina Grzywa <grzywakarolina@gmail.com>
2022-06-22 00:25:55 +0200

    Add task 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
 console.log('DOM');
+
+const linksList = document.querySelectorAll('a[data-url]');
+
+for (let i=0; i<linksList.length; i++) {
+    let link = linksList[i].dataset.url
+    linksList[i].href = link
+}
+
+
+// const links = document.querySelectorAll('a[data-url]')
+// for (let i=0;i<links.length;i++) {
+//     let link = links[i].dataset.url
+//     links[i].href = link
