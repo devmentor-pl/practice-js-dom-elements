77f8fa05908893b6c96c0586f747903bbc90b712
Karolina Grzywa <grzywakarolina@gmail.com>
2022-06-22 01:16:49 +0200

    Add task 5

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,11 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newButton = document.createElement('button');
+newButton.textContent = 'Usuń z koszyka';
+const article = curr.parentElement;
+article.append(newButton);
+
+const child = domArticle.children;
+
