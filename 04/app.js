8347d1e560f5fc253d5497f07af8a24d47762a5a
Karolina Grzywa <grzywakarolina@gmail.com>
2022-06-22 01:10:07 +0200

    Add task 4

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,21 @@
 console.log('DOM');
 
+const nav = document.querySelector('nav');
+const ul = document.createElement('ul');
+nav.appendChild(ul)
+
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+    { text: 'start', url: '/' },
+    { text: 'galeria', url: '/gallery' },
+    { text: 'kontakt', url: '/contact' },
 ];
+
+menuItems.forEach((element) => {
+    const li = document.createElement('li')
+    ul.appendChild(li)
+    const a = document.createElement('a')
+    li.appendChild(a)
+    a.href = element.url
+    a.textContent = element.text
+})
