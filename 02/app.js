64138d50ab68a176ed8c8ea7294e5bdbe27ae07f
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-25 16:18:34 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
-console.log('DOM');
+console.log("DOM");
+
+const links = document.querySelectorAll("[data-url]");
+console.log(links);
+links.forEach(function (el) {
+	const link = el.getAttribute("data-url");
+	el.setAttribute("href", link);
+});
