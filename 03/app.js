d1dfbf3278f5345db758590ca88b70245598d49e
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-25 16:24:30 +0200

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,30 @@
-console.log('DOM');
+console.log("DOM");
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+	attr: {
+		className: "btn",
+		title: "super button",
+	},
+	css: {
+		border: "1px solid #336699",
+		padding: "5px 20px",
+		color: "#444",
+	},
+	text: "Click me!",
+};
+const sectionElement = document.querySelector(".parent-for-button");
+if (sectionElement) {
+	const newButton = document.createElement("button");
+
+	for (const key in buttonSettings.attr) {
+		newButton.setAttribute(key, buttonSettings.attr[key]);
+	}
+
+	for (const key in buttonSettings.css) {
+		newButton.style[key] = buttonSettings.css[key];
+	}
+
+	newButton.innerText = buttonSettings.text;
+
+	sectionElement.appendChild(newButton);
 }
