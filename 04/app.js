2a8e9cc20ae643c32b9aa5c7ee02875e304f0029
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-25 16:43:07 +0200

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,21 @@
-console.log('DOM');
+console.log("DOM");
+
+const navElement = document.querySelector("nav");
+const ulElement = document.createElement("ul");
+navElement.appendChild(ulElement);
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+	{ text: "start", url: "/" },
+	{ text: "galeria", url: "/gallery" },
+	{ text: "kontakt", url: "/contact" },
 ];
+
+menuItems.forEach((element) => {
+	const liElement = document.createElement("li");
+	ulElement.appendChild(liElement);
+	const aElement = document.createElement("a");
+	liElement.appendChild(aElement);
+	aElement.href = element.url;
+	aElement.textContent = element.text;
+});
