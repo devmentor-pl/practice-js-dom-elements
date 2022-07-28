36cbe85fcad6b954cc0352904d2a3988484497c8
Joanna Markiewicz <jmarkiewicz1@op.pl>
2022-07-28 13:26:50 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const linksList = document.querySelectorAll('[data-url')
+console.log(linksList)
+
+linksList.forEach(function (el) {
+    const attr = el.getAttribute('data-url');
+    el.setAttribute('href', attr)
+
+});
