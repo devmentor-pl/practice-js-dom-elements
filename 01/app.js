f5b154f5475266d98331704a9b51dc89e6c219e7
Jacek Zielinski <gandi34@wp.pl>
2022-09-17 18:27:44 +0200

    01 dom elements

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,14 @@
-console.log('DOM');
+console.log ('DOM');
+
+const classElement = document.querySelector('.comments');
+console.log (classElement);
+
+const firstClass = document.getElementsByClassName('.comments__item');
+console.log (firstClass);
+
+const secondClass = document.getElementsByClassName('.comments__item--newest');
+console.log(secondClass);
+
+
+
+
