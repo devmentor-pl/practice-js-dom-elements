4571181460e5377701a5d75498b6604cc95381a2
Jacek Zielinski <gandi34@wp.pl>
2022-09-18 18:07:44 +0200

    02 js dom elements

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,24 @@
 console.log('DOM');
+/*Todo List*/
+
+/*1.Wyszukanie linków z atrybutami data-url.*/
+const links = document.querySelectorAll('a');
+    console.log (links);
+
+const arr = Array.from(links);
+    console.log (arr);
+
+
+
+
+/*2.pobranie zawartości tego atrybutu do zmiennej.*/
+
+
+
+
+/*3.Przypisanie tej zmiennej do atrybutu href wyszukanego elementu <a/>.
+
+4.Sprawdzenie,czy każdy z linków działa prawidłowo,czyli czy
+przekierowuje do odpowiedniej strony.
+Ostatni element na liście nie zawiera przekierowania.*/
+
