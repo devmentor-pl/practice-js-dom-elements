79956a1a8f592eedcd6db62195dea70881219ed4
Jacek Zielinski <gandi34@wp.pl>
2022-09-19 23:09:31 +0200

    02 js dom elements

--- 02/app.js
+++ 02/app.js
@@ -9,14 +9,16 @@ const arr = Array.from(links);
     console.log (arr);
 
 
-
-
 /*2.pobranie zawartości tego atrybutu do zmiennej.*/
 
-
+const dataAtrribute = arr[0].dataset.url;
+const dataAtrributeMozilla = arr[1].dataset.url;
+const dataAtrributeStack = arr[2].dataset.url;
+console.log (dataAtrribute,dataAtrributeMozilla,dataAtrributeStack);
 
 
 /*3.Przypisanie tej zmiennej do atrybutu href wyszukanego elementu <a/>.
+
 
 4.Sprawdzenie,czy każdy z linków działa prawidłowo,czyli czy
 przekierowuje do odpowiedniej strony.
