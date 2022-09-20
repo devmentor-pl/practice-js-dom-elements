1274cc794b6bbc2bce76acaa03b2051350e6b977
Jacek Zielinski <gandi34@wp.pl>
2022-09-20 23:08:08 +0200

    03 js dom elements

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,34 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+
+
+
+
+const buttonParent = document.querySelector('.parent-for-button');
+
+if (buttonParent) {
+  const button = document.createElement('button')
+  newBtn = document.createElement('button')
+  buttonParent.appendChild (newBtn)
+    for (keys in buttonSettings) {
+      const value = buttonSettings[keys]
+      if(keys === "text") 
+        newBtn.innerHTML = value
+      for (keys in value) {
+        if(keys === 'className') 
+          newBtn.classList.add(value[keys])
+        if(keys === 'title') 
+          newBtn.dataset.title = value[keys]
+        if(keys === 'border') 
+          newBtn.style.border = value[keys]
+        if(keys === 'padding')
+          newBtn.style.padding = value[keys]
+        if(keys === 'color')
+          newBtn.style.color = value[keys]
+      }
+   }
+}
+
+console.log(buttonParent)
