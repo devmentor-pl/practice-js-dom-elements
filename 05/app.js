b1c253d46c6463706160173da0321f58e27a7d3b
Jacek Zielinski <gandi34@wp.pl>
2022-09-24 12:02:18 +0200

    05 js dom elements TO DO LIST

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,22 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+
+/*Todo List
+
+1.Utworzyć kolejny przycisk,będący rodzeństem dal elementu ze zmiennej 'curr',
+element ten ma zawierać napis 'usuń z koszyka'.
+
+2.Dodaj klasę '.siblings' do wszystkich elementów klasy .'js-curr',
+wykorzystując pętle.
+
+3.Do elementu o klasie '.article',który występuje po rodzicu elementu '.js-curr',
+dodać atrybut 'title' o wartości 'nextElementSibling'.
+
+4.Dodaj dodatkowy paragraf do ostatniego artykuł i umieścić go przed znacznikiem '<button/>'.
+
+5.Na początku listy dodać kolejny artykuł,który będzie miał tę samą strukturę,co pozostałe artykuły.
+*/
+
+
