910264fe48d35cea269695bd1bca8ad466278c0b
Anna <aniamatrackasadurek@gmail.com>
2022-11-18 12:49:11 +0100

    exercise 1 finished

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,13 @@
 console.log('DOM');
+
+const pEl = document.querySelector('.comments__item.comments__item--newest');
+
+console.log(pEl);
+
+const data = pEl.querySelectorAll('[data-info]');
+
+data.forEach(function(element, index) {
+    element.textContent = data[index];
+});
+
+console.log(data.length);
