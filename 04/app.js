e67d51e144f4bf93764a1c5689116139f98547a6
Anna <aniamatrackasadurek@gmail.com>
2022-11-21 06:47:47 +0100

    exercise 4 finished: solution with forEach

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,26 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const navEl = document.querySelector('nav');
+const ulEl = document.createElement('ul');
+
+if(navEl) {
+    navEl.appendChild(ulEl);
+
+    for(let i = 0; i < menuItems.length; i++) {
+        let liEl = document.createElement('li');
+        let aEl = document.createElement('a');
+
+        ulEl.appendChild(liEl);
+        liEl.appendChild(aEl);
+    }
+}
+
+const aElItems = ulEl.querySelectorAll('a');
+
+aElItems.forEach(function(element, index) {
+    element.setAttribute('href', menuItems[index]['url']);
+    element.textContent = menuItems[index]['text'];
+})
+
