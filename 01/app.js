e1c01f6bf37954f7f77d2e9c93f6e720a9907338
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-28 21:21:45 +0100

    Finish task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const elDoubleClass = document.querySelector(
+	'.comments__item.comments__item--newest'
+);
+
+const elDataList = elDoubleClass.querySelectorAll('[data-info]');
+console.log(elDataList);
+
+console.log(elDataList.length);
