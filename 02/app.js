1a6e2797e5c8a1addd975d81dbda09741c78cac3
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-29 20:42:24 +0100

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,12 @@
 console.log('DOM');
+
+const linkList = document.querySelectorAll('[data-url]');
+
+if (linkList) {
+	linkList.forEach(function (item) {
+		const hrefValue = item.getAttribute('data-url');
+		if (!item.hasAttribute('href')) {
+			item.setAttribute('href', hrefValue);
+		}
+	});
+}
