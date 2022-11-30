b32d220c413ec8d5ae04d5e446653f68b17bfd65
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-11-30 20:29:12 +0100

    03 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,23 @@
-console.log('DOM');
+const linkList = document.querySelectorAll('[data-url]');
+const attributeList = [];
+const a = [...document.querySelectorAll('a')];
+let count = 0;
+
+linkList.forEach(function(item) {
+    attributeList.push(item.getAttribute('data-url'))
+})
+
+a.forEach(function(item){
+    
+    if(attributeList[count] === undefined) {
+        item.setAttribute('href', '#');
+    }
+    else {
+        item.setAttribute('href', attributeList[count]);
+    }
+    count++;
+})
+
+// for(let i = 0; i < attributeList.length; i++) {
+//     a[i].setAttribute('href', attributeList[i])
+// }
