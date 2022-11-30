69cf833c6025540cc185034820ec519d7b17ec99
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-30 17:47:41 +0100

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,26 @@
 console.log('DOM');
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+	attr: {
+		className: 'btn',
+		title: 'super button',
+	},
+	css: {
+		border: '1px solid #336699',
+		padding: '5px 20px',
+		color: '#444',
+	},
+	text: 'Click me!',
+};
+
+const section = document.querySelector('.parent-for-button');
+
+const btn = document.createElement('button');
+for (const prop in buttonSettings.attr) {
+	btn.setAttribute(prop, buttonSettings.attr[prop]);
 }
+for (const prop in buttonSettings.css) {
+	btn.style[prop] = buttonSettings.css[prop];
+}
+btn.innerText = buttonSettings.text;
+section.appendChild(btn);
