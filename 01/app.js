e817a98775071a5585509dedd083fa604f3c4199
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-11-30 19:12:00 +0100

    01 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,4 @@
 console.log('DOM');
+const doubleClass = document.querySelector('.comments__item.comments__item--newest');
+const dataInfo = doubleClass.querySelectorAll('[data-info]');
+console.log(`Znaleziono: ${dataInfo.length} elementy`)
