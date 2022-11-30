18ee0f0c56659fdac22494cd22d341e2a38818fb
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-30 18:13:31 +0100

    task 03 - button

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,28 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+ 
+const btnParent = document.querySelector('.parent-for-button');
+
+if(btnParent) {
+    const btn = document.createElement('button'); 
+
+    if(buttonSettings.attr) {
+        for(item in buttonSettings.attr) {
+            btn.className = buttonSettings.attr.className;
+            btn.setAttribute('title', buttonSettings.attr.title)
+        } 
+        for (item in buttonSettings.css) {
+            btn.style.border = buttonSettings.css.border
+            btn.style.padding = buttonSettings.css.padding
+            btn.style.color = buttonSettings.css.color
+        }
+    }
+    
+    if (buttonSettings.text) { 
+        btn.innerHTML = buttonSettings.text; 
+    }
+
+    btnParent.appendChild(btn)
+    console.log(btnParent.innerHTML)
+}
