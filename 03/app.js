b9ce94180eff532bf9bddb4ccf1db3f057813723
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-12-01 18:56:05 +0100

    done but wanna better

--- 03/app.js
+++ 03/app.js
@@ -13,44 +13,25 @@ const buttonSettings = {
 const btn = document.createElement('button');
 const btnParrent = document.querySelector('.parent-for-button');
 btnParrent.appendChild(btn);
-const keysList = [];
-const valuesList = [];
 
-const isObject = function(key) {
-    if(key === null) {
-        return false;
+
+
+for(const loop in buttonSettings) {
+    if(loop === 'attr') {
+       const attrKeys = Array.from(Object.keys(buttonSettings.attr)).flat();
+       const attrValues = Array.from(Object.values(buttonSettings.attr)).flat();
+        for(let i = 0; i < attrKeys.length; i++){
+            btn.setAttribute(attrKeys[i], attrValues[i])
+        }
     }
-    return (typeof key === "object");
+    if(loop === 'css') {
+       const cssKeys = Array.from(Object.keys(buttonSettings.css)).flat();
+       const cssValues = Array.from(Object.values(buttonSettings.css)).flat();
+        for(let i = 0; i < cssKeys.length; i++){
+            btn.style.setProperty(cssKeys[i], cssValues[i])
+        }
+    }
+    if(loop === 'text') {
+        btn.innerText = buttonSettings.text;
+    }
 }
-
-for(const key in buttonSettings) {
-    keysList.push(key)
-    valuesList.push(key);
-    if(isObject(buttonSettings[key])) {
-        for(const key2 in buttonSettings[key]) {
-            keysList.push(key2)
-            valuesList.push(buttonSettings[key][key2]);
-        }
-        } else {
-            valuesList.push(buttonSettings[key]);
-        }
-    }
-
-    const arr = [];
-    for(let i = 0; i<valuesList.length; i++) {
-        if(valuesList[i].includes('attr')) {
-            i++;
-            console.log(keysList[i], valuesList[i]);
-            btn.setAttribute(keysList[i], valuesList[i]); 
-        }
-        if(valuesList[i].includes('css')) {
-            i++;
-            console.log(keysList[i], valuesList[i]);
-            btn.style.setProperty(keysList[i], valuesList[i]);
-        }
-        if(valuesList.includes("text")) {
-            console.log(keysList[i], valuesList[i]);
-            btn.innerText = valuesList[i]
-        }
-        else console.log('jakis blad')
-    }
