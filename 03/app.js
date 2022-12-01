2e7c6e311d39309bb171f612e180a830cccaa594
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-12-01 16:13:09 +0100

    03 done, but please give me better idea

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,3 @@
-console.log('DOM');
-
 const buttonSettings = {
     attr: {
         className: 'btn',
@@ -12,3 +10,47 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+const btn = document.createElement('button');
+const btnParrent = document.querySelector('.parent-for-button');
+btnParrent.appendChild(btn);
+const keysList = [];
+const valuesList = [];
+
+const isObject = function(key) {
+    if(key === null) {
+        return false;
+    }
+    return (typeof key === "object");
+}
+
+for(const key in buttonSettings) {
+    keysList.push(key)
+    valuesList.push(key);
+    if(isObject(buttonSettings[key])) {
+        for(const key2 in buttonSettings[key]) {
+            keysList.push(key2)
+            valuesList.push(buttonSettings[key][key2]);
+        }
+        } else {
+            valuesList.push(buttonSettings[key]);
+        }
+    }
+
+    const arr = [];
+    for(let i = 0; i<valuesList.length; i++) {
+        if(valuesList[i].includes('attr')) {
+            i++;
+            console.log(keysList[i], valuesList[i]);
+            btn.setAttribute(keysList[i], valuesList[i]); 
+        }
+        if(valuesList[i].includes('css')) {
+            i++;
+            console.log(keysList[i], valuesList[i]);
+            btn.style.setProperty(keysList[i], valuesList[i]);
+        }
+        if(valuesList.includes("text")) {
+            console.log(keysList[i], valuesList[i]);
+            btn.innerText = valuesList[i]
+        }
+        else console.log('jakis blad')
+    }
