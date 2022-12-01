7a79ccdf8bbb4ecf6e6823d00426f7da17667256
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-12-01 21:47:04 +0100

    04 done

--- 04/app.js
+++ 04/app.js
@@ -1,4 +1,19 @@
-console.log('DOM');
+const nav = document.querySelector('nav');
+const ul = document.createElement('ul');
+const liList = [];
+const aList = [];
+
+for(let i = 0; i<3; i++) {
+    liList[i] = document.createElement('li');
+    aList[i] = document.createElement('a')
+    ul.appendChild(liList[i]);
+    liList[i].appendChild(aList[i]);
+}
+nav.appendChild(ul);
+
+
+
+
 
 // struktura do wykorzystania w pętli
 const menuItems = [
@@ -6,3 +21,17 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+aList.forEach(function(item, i) {
+    item.setAttribute('href', menuItems[i].url);
+    item.innerText = menuItems[i].text;
+})
+
+
+// aList[0].setAttribute('href', '/home')
+// aList[1].setAttribute('href', '/gallery')
+// aList[2].setAttribute('href', 'contact')
+
+// aList[0].innerText = 'home'
+// aList[1].innerText = 'galeria'
+// aList[2].innerText = 'kontakt'
