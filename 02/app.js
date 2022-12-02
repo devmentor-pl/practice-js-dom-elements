10531d93bf4f00d413c4c7459ac174837dc7599e
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-12-02 15:17:54 +0100

    Delete if

--- 02/app.js
+++ 02/app.js
@@ -2,11 +2,9 @@ console.log('DOM');
 
 const linkList = document.querySelectorAll('[data-url]');
 
-if (linkList) {
-	linkList.forEach(function (item) {
-		const hrefValue = item.dataset.url;
-		if (!item.hasAttribute('href')) {
-			item.setAttribute('href', hrefValue);
-		}
-	});
-}
+linkList.forEach(function (item) {
+	const hrefValue = item.dataset.url;
+	if (!item.hasAttribute('href')) {
+		item.setAttribute('href', hrefValue);
+	}
+});
