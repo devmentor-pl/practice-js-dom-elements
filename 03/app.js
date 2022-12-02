67b8aeb0631bc6101fb1e04fd7ad8c1382696d99
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-12-02 17:16:16 +0100

    03 done

--- 03/app.js
+++ 03/app.js
@@ -15,23 +15,33 @@ const btnParrent = document.querySelector('.parent-for-button');
 btnParrent.appendChild(btn);
 
 
-
-for(const loop in buttonSettings) {
-    if(loop === 'attr') {
-       const attrKeys = Array.from(Object.keys(buttonSettings.attr)).flat();
-       const attrValues = Array.from(Object.values(buttonSettings.attr)).flat();
-        for(let i = 0; i < attrKeys.length; i++){
-            btn.setAttribute(attrKeys[i], attrValues[i])
-        }
-    }
-    if(loop === 'css') {
-       const cssKeys = Array.from(Object.keys(buttonSettings.css)).flat();
-       const cssValues = Array.from(Object.values(buttonSettings.css)).flat();
-        for(let i = 0; i < cssKeys.length; i++){
-            btn.style.setProperty(cssKeys[i], cssValues[i])
-        }
-    }
-    if(loop === 'text') {
-        btn.innerText = buttonSettings.text;
-    }
+for(const attrName in buttonSettings.attr){
+    btn.setAttribute(attrName, buttonSettings.attr[attrName]);
 }
+for(const cssName in buttonSettings.css){
+    btn.style.setProperty(cssName, buttonSettings.css[cssName]);
+}
+for(const textName in buttonSettings.text){
+    btn.textContent = buttonSettings.text;
+
+}
+
+// for(const loop in buttonSettings) {
+//     if(loop === 'attr') {
+//        const attrKeys = Array.from(Object.keys(buttonSettings.attr)).flat();
+//        const attrValues = Array.from(Object.values(buttonSettings.attr)).flat();
+//         for(let i = 0; i < attrKeys.length; i++){
+//             btn.setAttribute(attrKeys[i], attrValues[i])
+//         }
+//     }
+//     if(loop === 'css') {
+//        const cssKeys = Array.from(Object.keys(buttonSettings.css)).flat();
+//        const cssValues = Array.from(Object.values(buttonSettings.css)).flat();
+//         for(let i = 0; i < cssKeys.length; i++){
+//             btn.style.setProperty(cssKeys[i], cssValues[i])
+//         }
+//     }
+//     if(loop === 'text') {
+//         btn.innerText = buttonSettings.text;
+//     }
+// }
