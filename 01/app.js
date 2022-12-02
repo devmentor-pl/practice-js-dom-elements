99b2ae4df0028f697232fd9cd18ba6cc2946db89
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-12-02 13:31:45 +0100

    01 fixed

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,9 @@
 console.log('DOM');
 const doubleClass = document.querySelector('.comments__item.comments__item--newest');
-const dataInfo = doubleClass.querySelectorAll('[data-info]');
-console.log(`Znaleziono: ${dataInfo.length} elementy`)
+if(doubleClass) {
+    const dataInfo = doubleClass.querySelectorAll('[data-info]');
+    console.log(`Znaleziono: ${dataInfo.length} elementy`)
+}
+else {
+    console.log("nie znaleziono elementu");
+}
