64c8276913ab69a84410ee0958e7022a43bd9742
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-12-02 20:33:17 +0100

    first part of task

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,58 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const navEl = document.querySelector('nav');
+
+//wersja z pętlą
+
+
+
+// wersja bez pętli
+
+
+if(navEl) {
+    const newUl = document.createElement('ul');
+    navEl.appendChild(newUl);
+    console.log(newUl.innerHTML);
+    
+    if(newUl) {
+        const newLi = document.createElement('li');
+        newUl.appendChild(newLi)
+        console.log(newLi.innerHTML)
+    
+        if(newLi) {
+            const link = document.createElement('a');
+
+            if(!link.hasAttribute("href")) {
+                link.setAttribute("href", "/");
+                link.innerText = 'start';
+            }
+            newLi.appendChild(link);
+            console.log(newLi.innerHTML);
+        }
+    }
+
+    const newLi = document.querySelector("li");
+    const newLi2 = newLi.cloneNode(true);
+    const newLi3 = newLi.cloneNode(true);
+    newUl.appendChild(newLi2);
+    newUl.appendChild(newLi3);
+    
+    if(newLi2) {
+        const link2 = document.querySelector("li:nth-child(2) > a ");
+        link2.setAttribute("href", "/gallery");
+        link2.innerText = 'galeria';
+    }
+
+    if(newLi3) {
+        const link2 = document.querySelector("li:nth-child(3) > a ");
+        link2.setAttribute("href", "/contact");
+        link2.innerText = 'kontakt';
+    }
+}
+
+
+
+
+
