931d6db5002557be9268834cba4b044129bbcfdd
Michal Lata <maxpoz3212@gmail.com>
2022-12-29 21:00:31 +0100

    Complete task01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const liEl = document.querySelector('.comments__item.comments__item--newest');
+
+if (liEl) {
+    const liElAttribute = liEl.querySelectorAll('[data-info]');
+
+    if (liElAttribute) {
+        console.log(liElAttribute.length);
+    }
+}
