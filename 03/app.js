0495ee529a5909f7f83aee007f7a63ba62d03456
Michal Lata <maxpoz3212@gmail.com>
2022-12-29 22:06:15 +0100

    Complete 50% task

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,42 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const btnParent = document.querySelector('.parent-for-button');
+
+if (btnParent) {
+    const btn = document.createElement('button');
+
+    for (const key in buttonSettings.attr) {
+        btn.setAttribute(key, buttonSettings.attr[key]);
+    }
+    for (const key in buttonSettings.css) {
+
+        btn.style.key = buttonSettings.css[key];
+        // btn.setAttribute('style', key + buttonSettings.css[key])
+    }
+    btn.innerText = buttonSettings.text;
+
+    // for (const key in buttonSettings) {
+
+    //     console.log(buttonSettings[key]);
+
+    //     console.log(Object(buttonSettings).value);
+
+    // btn.setAttribute(buttonSettings[key], key)
+
+    // if (key === 'attr') {
+    //     for (const prop in buttonSettings[key]) {
+    //         btn.setAttribute(prop, buttonSettings[key][prop])
+    //     }
+    // } else if (key === 'css') {
+    //     for (const prop in buttonSettings[key]) {
+    //         btn.style.prop = buttonSettings[key][prop];
+    //     }
+    // }
+    btnParent.appendChild(btn);
+    console.log(btn);
+}
+
+
+
