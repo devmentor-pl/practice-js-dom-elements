2ebd430627e37379a0313279ee512e0ff253062b
Michal Lata <maxpoz3212@gmail.com>
2022-12-29 21:10:55 +0100

    Complete task02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const linkList = [...document.querySelectorAll('[data-url]')];
+
+if (linkList) {
+
+    linkList.forEach(function (item) {
+        const itemAttr = item.getAttribute('data-url');
+        item.setAttribute('href', itemAttr);
+    })
+};
