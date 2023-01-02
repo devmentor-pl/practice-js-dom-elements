2d0a58fbb8963a2cdb81cef0954ed82a6860141b
Michal Lata <maxpoz3212@gmail.com>
2023-01-02 09:48:53 +0100

    Add fix in 02

--- 02/app.js
+++ 02/app.js
@@ -2,10 +2,7 @@ console.log('DOM');
 
 const linkList = [...document.querySelectorAll('[data-url]')];
 
-if (linkList) {
-
-    linkList.forEach(function (item) {
-        const itemAttr = item.getAttribute('data-url');
-        item.setAttribute('href', itemAttr);
-    })
-};
+linkList.forEach(function (item) {
+    const itemAttr = item.getAttribute('data-url');
+    item.setAttribute('href', itemAttr);
+})
