df82b41c4b1841b49358109ea45e0d5b241e16f4
Michal Lata <maxpoz3212@gmail.com>
2023-01-02 09:46:51 +0100

    Add fix in 01

--- 01/app.js
+++ 01/app.js
@@ -5,7 +5,6 @@ const liEl = document.querySelector('.comments__item.comments__item--newest');
 if (liEl) {
     const liElAttribute = liEl.querySelectorAll('[data-info]');
 
-    if (liElAttribute) {
-        console.log(liElAttribute.length);
-    }
+    console.log(liElAttribute.length);
+
 }
