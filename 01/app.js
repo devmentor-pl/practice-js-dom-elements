6c4bb24de419b32d31ef0dccadfcd4a5df4cd625
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-01-24 11:46:40 +0100

    Ad ex 1

--- 01/app.js
+++ 01/app.js
@@ -7,7 +7,7 @@ const commentsElement = document.querySelector(
 console.log(commentsElement);
 
 if (commentsElement) {
-	const dataElements = commentsElement.querySelectorAll("[data-info");
+	const dataElements = commentsElement.querySelectorAll("[data-info]");
 	console.log(dataElements);
 	console.log(dataElements.length);
 }
