4468b914972e608d29329987c8f86d97aae9e0d9
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-02-06 21:03:58 +0100

    add task-04

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,31 @@
-console.log('DOM');
+console.log('DOM')
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
-];
+	{ text: 'start', url: '/' },
+	{ text: 'galeria', url: '/gallery' },
+	{ text: 'kontakt', url: '/contact' },
+]
+
+const nav = document.querySelector('nav')
+const navList = document.createElement('ul')
+
+// nav.innerHTML = `
+//     <ul>
+//         <li><a href="/">home</a></li>
+//         <li><a href="/gallery">gallery</a></li>
+//         <li><a href="/contact">contact</a></li>
+//     </ul>
+//     `
+
+// z użyciem pętli forEach
+menuItems.forEach(function (el) {
+	const navItem = document.createElement('li')
+	const navLink = document.createElement('a')
+	navLink.innerText = el.text
+	navLink.setAttribute('href', el.url)
+	navList.appendChild(navItem)
+	navItem.append(navLink)
+})
+
+nav.appendChild(navList)
