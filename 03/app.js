c3c5c61f1f2b262b14eb8ccba1d3b16636f07f61
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2023-02-06 17:13:23 +0100

    add task-03

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,28 @@
-console.log('DOM');
+console.log('DOM')
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+	attr: {
+		className: 'btn',
+		title: 'super button',
+	},
+	css: {
+		border: '1px solid #336699',
+		padding: '5px 20px',
+		color: '#444',
+	},
+	text: 'Click me!',
 }
+
+const parentForBtn = document.querySelector('.parent-for-button')
+const btn = document.createElement('button')
+
+for (const key in buttonSettings.attr) {
+	const value = buttonSettings.attr[key]
+	btn[key] = value
+}
+for (const key in buttonSettings.css) {
+	const value = buttonSettings.css[key]
+	btn.style[key] = value
+}
+btn.innerText = buttonSettings.text
+parentForBtn.appendChild(btn)
