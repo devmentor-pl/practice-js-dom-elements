045dff6044bf5b5345e217bfd474e8a8205706e0
anetasobanska <sannettka@gmail.com>
2023-02-17 17:25:16 +0100

    Finished #03

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,19 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const section = document.querySelector('.parent-for-button');
+if(section) {
+    const newButton = document.createElement('button');
+    for (const key in buttonSettings) {
+        newButton.classList.add(buttonSettings[key].className);
+        if(!newButton.hasAttribute('title')) {
+            newButton.setAttribute('title', buttonSettings[key].title);
+        }
+        newButton.style.border = buttonSettings[key].border;
+        newButton.style.padding = buttonSettings[key].padding;
+        newButton.style.color = buttonSettings[key].color;
+        newButton.innerText = buttonSettings.text;
+    }
+    section.appendChild(newButton);
+    }
