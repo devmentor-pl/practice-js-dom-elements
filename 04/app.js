25f77d67aac9cb5627036051dc20cea6a1c7bce6
anetasobanska <sannettka@gmail.com>
2023-02-18 11:32:42 +0100

    Finished #04

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,22 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+const ul = document.createElement('ul');
+
+menuItems.forEach(el => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    if(!a.hasAttribute('href')) {
+        a.setAttribute('href', `${el.url}`)
+        a.innerText = `${el.text}`;
+    }
+    ul.appendChild(li);
+    li.appendChild(a);    
+})
+
+if(nav) {
+    nav.appendChild(ul);
+}
+console.log(nav);
