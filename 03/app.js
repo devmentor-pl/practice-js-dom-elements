3d6e4bf8846b43be13266f886992b458318a989a
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-03-11 14:00:09 +0100

    Finished task 3

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,20 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+
+const sectionEl = document.querySelector('.parent-for-button');
+const buttonEl = document.createElement('button');
+
+if(sectionEl){
+    sectionEl.appendChild(buttonEl);
+
+for(const key in buttonSettings){
+    buttonEl.textContent = buttonSettings[key];
+}
+for(const key in buttonSettings.attr){
+    buttonEl.setAttribute(key, buttonSettings.attr[key])
+}
+for(const key in buttonSettings.css){
+    buttonEl.style.setProperty(key, buttonSettings.css[key]) 
+}};
