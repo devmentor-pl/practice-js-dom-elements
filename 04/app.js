6a17057c26ab4dcaa85fa37240d9803549d301f2
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-03-11 15:01:40 +0100

    Task 4 first solution

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,31 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+const unordList = document.createElement('ul');
+const listElem = document.createElement('li')
+const aElement = document.createElement('a');
+
+nav.appendChild(unordList);
+unordList.appendChild(listElem);
+listElem.appendChild(aElement);
+
+aElement.setAttribute('href', '/');
+aElement.textContent = 'start';
+
+const listElem2 = listElem.cloneNode(true);
+const aElement2 = listElem2.querySelector('a');
+
+aElement2.setAttribute('href', '/gallery');
+aElement2.textContent = 'gallery';
+
+unordList.appendChild(listElem2);
+
+const listElem3 = listElem.cloneNode(true);
+const aElement3 = listElem3.querySelector('a');
+
+aElement3.setAttribute('href', '/contact');
+aElement3.textContent = 'contact';
+
+unordList.appendChild(listElem3);
