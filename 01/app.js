97f282490a4cca9934f64065f1f3bdafa7765b96
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-03-11 12:57:57 +0100

    Finished task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const liElList = document.querySelector('.comments__item.comments__item--newest');
+
+if(liElList){
+    const dataInfoEl = liElList.querySelectorAll('[data-info]');
+    console.log(dataInfoEl.length)
+}
