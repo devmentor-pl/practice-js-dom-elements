ffd8611bc9353258f5297f7b184747782607d3aa
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-04-02 14:17:22 +0200

    finished task

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,11 @@
-console.log('DOM');
+console.log("DOM");
+
+const links = document.querySelectorAll("a[data-url]");
+if (links) {
+  links.forEach((link) => {
+    if (link) {
+      const url = link.getAttribute("data-url");
+      link.setAttribute("href", url);
+    }
+  });
+}
