b8ba56c3a4f7b67647492b9d0ccbfc56ec4f8017
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2023-04-02 13:56:27 +0200

    finished task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
-console.log('DOM');
+console.log("DOM");
+
+const item = document.querySelector(".comments__item.comments__item--newest");
+if (item) {
+  const dataInfoElements = document.querySelectorAll("[data-info]");
+  console.log(
+    `Znaleziono ${dataInfoElements.length} elementów z atrybutem [data-info]`
+  );
+}
