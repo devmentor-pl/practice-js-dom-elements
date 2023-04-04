d1bac7f27f991a11c7140a4c6d412612cf5f6b5d
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-04 23:19:30 +0200

    Added task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,16 @@
 console.log('DOM');
+
+const twoClasses = document.querySelector('.comments__item.comments__item--newest');
+
+if (twoClasses) {
+    const spanList = twoClasses.querySelectorAll('span');
+
+    let counter = 0;
+    for (let i = 0; i < spanList.length; i++) {
+        if (spanList[i].dataset.info) {
+            counter++;
+        }
+    }
+    console.log(counter);
+
+}
