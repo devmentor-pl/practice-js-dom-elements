f32d6efed5c1fbdcc35cbd62b6cc51554ad51cb2
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-05 11:13:15 +0200

    Added task 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const aList = document.querySelectorAll('a');
+if (aList) {
+    for (let i = 0; i < aList.length; i++) {
+    if (aList[i].hasAttribute('data-url')) {
+            aList[i].setAttribute('href', aList[i].getAttribute('data-url'));
+        }
+    }
+}
