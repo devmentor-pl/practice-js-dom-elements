1e5621aafefead8dfa470bbd11127e922a8ac230
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-05 14:05:20 +0200

    Added task 4 bad version

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,26 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const ulElement = document.createElement('ul');
+const navElement = document.querySelector('nav');
+navElement.appendChild(ulElement);
+
+const liList = [];
+const aList = [];
+for (let i = 0; i < 3; i++) {
+    liList[i] = document.createElement('il');
+    aList[i] = document.createElement('a');;
+    ulElement.appendChild(liList[i]);
+    liList[i].appendChild(aList[i]);
+}
+
+aList[0].setAttribute('href', '/');
+aList[1].setAttribute('href', '/gallery');
+aList[2].setAttribute('href', '/content');
+
+aList[0].textContent = 'home';
+aList[1].innerText = 'gallery';
+aList[2].innerText = 'content';
+
+
