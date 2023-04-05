3736ee029f4b1da78159225630616e3bf03e8f0f
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-05 13:17:57 +0200

    Added task 3

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,30 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const newButton = document.createElement('button');
+const arr = [];
+for (const key1 in buttonSettings) {
+
+    const insideButtonSettings = buttonSettings[key1];
+    for (const key2 in insideButtonSettings) {
+
+        if (key1 === 'attr') {
+            newButton.setAttribute(key2, insideButtonSettings[key2]);
+        }
+        if (key1 === 'css') {
+            arr.push(key2 + ':' + insideButtonSettings[key2])
+        }
+        
+    }
+    if (arr.length !== 0 && !newButton.hasAttribute('style')) {
+        const newArr = arr.join(';')
+        newButton.setAttribute('style', newArr);
+    }
+    if (key1 === 'text') {
+        newButton.innerText = buttonSettings[key1];
+    }
+}
+
+const buttonParent = document.querySelector('.parent-for-button');
+buttonParent.appendChild(newButton);
