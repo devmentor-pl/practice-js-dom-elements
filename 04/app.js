f7888f549e55224cb30484bc4f8bdbd4334d0a69
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-04-06 12:04:02 +0200

    Added task 4 all

--- 04/app.js
+++ 04/app.js
@@ -2,30 +2,49 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+    { text: 'start', url: '/' },
+    { text: 'galeria', url: '/gallery' },
+    { text: 'kontakt', url: '/contact' },
 ];
+
+// const ulElement = document.createElement('ul');
+// const navElement = document.querySelector('nav');
+// navElement.appendChild(ulElement);
+
+// const liList = [];
+// const aList = [];
+// for (let i = 0; i < 3; i++) {
+//     liList[i] = document.createElement('il');
+//     aList[i] = document.createElement('a');;
+//     ulElement.appendChild(liList[i]);
+//     liList[i].appendChild(aList[i]);
+// }
+
+// aList[0].setAttribute('href', '/');
+// aList[1].setAttribute('href', '/gallery');
+// aList[2].setAttribute('href', '/content');
+
+// aList[0].textContent = 'home';
+// aList[1].innerText = 'gallery';
+// aList[2].innerText = 'content';
 
 const ulElement = document.createElement('ul');
 const navElement = document.querySelector('nav');
 navElement.appendChild(ulElement);
 
-const liList = [];
-const aList = [];
-for (let i = 0; i < 3; i++) {
-    liList[i] = document.createElement('il');
-    aList[i] = document.createElement('a');;
-    ulElement.appendChild(liList[i]);
-    liList[i].appendChild(aList[i]);
-}
 
-aList[0].setAttribute('href', '/');
-aList[1].setAttribute('href', '/gallery');
-aList[2].setAttribute('href', '/content');
+menuItems.forEach(function (element) {
+    let liElement, aElement;
+    liElement = document.createElement('li');
+    aElement = document.createElement('a');
 
-aList[0].textContent = 'home';
-aList[1].innerText = 'gallery';
-aList[2].innerText = 'content';
+    const menuItemsIndex = menuItems.indexOf(element);
+    const urlElement = (menuItems[menuItemsIndex]);
 
+    aElement.setAttribute('href', urlElement.url)
+    aElement.innerText = urlElement.text;
 
+    ulElement.appendChild(liElement);
+    liElement.appendChild(aElement)
+
+});
