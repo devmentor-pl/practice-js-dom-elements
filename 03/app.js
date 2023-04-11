b46d97459a868a8d2f075fd3670d6767d8b9c919
Jakub Kasperkowicz <jakub.kasperkowicz@gmail.com>
2023-04-11 13:44:30 +0200

    ended 03

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,35 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const button = document.createElement('button');
+
+// Ustawianie atrybutów
+for (const key in buttonSettings.attr) {  
+
+//   button.setAttribute(key, buttonSettings.attr[key])
+
+    const value = buttonSettings.attr[key]
+    button[key] = value
+}
+
+// Ustawianie stylów
+for (const key in buttonSettings.css) {
+//   button.style[key] = buttonSettings.css[key];
+
+const value = buttonSettings.css[key] 
+button.style[key] = value
+}
+
+// Ustawianie tekstu
+// button.textContent = buttonSettings.text
+button.innerText = buttonSettings.text
+
+
+// Wstawianie przycisku do elementu o klasie .parent-for-button
+const parentElement = document.querySelector('.parent-for-button');
+parentElement.appendChild(button);
+
+// button.style.border = '1px solid red'
+// button.innerText = 'jakiś tekst'
+// button.className = 'klasa'
