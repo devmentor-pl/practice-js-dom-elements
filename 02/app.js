222dd257c5c365f89a2053a7a11a693bd0fe4721
Jakub Kasperkowicz <jakub.kasperkowicz@gmail.com>
2023-04-11 13:18:27 +0200

    ended 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,18 @@
 console.log('DOM');
+
+const searchDataUrl = document.querySelectorAll('[data-url]')
+
+// for (let i = 0; i < searchDataUrl.length; i++) {
+//     let element = searchDataUrl[i];
+
+//     let url = element.getAttribute('data-url');
+//     element.setAttribute('href', url);
+//     console.log([i+1] + '. ' + url);
+//   }
+
+  searchDataUrl.forEach(function(el){
+    const attr = el.getAttribute('data-url')
+
+    el.setAttribute('href', attr)
+  })
+  
