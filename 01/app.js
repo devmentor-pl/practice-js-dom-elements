5adc73b27c15e52b2d64394b6e6b575b59121aa1
kozerka <kozerka@gmail.com>
2023-04-12 23:14:03 +0200

    Complete task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const element = document.querySelector(
+	'.comments__item.comments__item--newest'
+);
+console.log(element);
+
+if (element) {
+	const element2 = element.querySelectorAll('[data-info]');
+	console.log(`Zostały znalezione ${element2.length} elementy`);
+}
