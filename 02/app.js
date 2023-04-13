d39e57c9a2d068c0149ee7ce2775d02b318b660b
Ryuuken <wachnickiandrzej@gmail.com>
2023-04-13 20:13:23 +0200

    Setting attribute value exercise done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const aElements = [...document.querySelectorAll('a')];
+if(aElements) {
+    aElements.forEach(function(el) {
+        if(el.hasAttribute('data-url')) {
+            el.setAttribute('href', el.getAttribute('data-url'))
+        }
+    })
+}
