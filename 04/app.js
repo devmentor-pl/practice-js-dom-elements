84fc9461950f528a307e4234d378c1b72ab7983e
Maria Kardas <mariah.grze@gmail.com>
2023-04-18 23:02:14 +0200

    First method

--- 04/app.js
+++ 04/app.js
@@ -1,5 +1,33 @@
 console.log('DOM');
 
+const navElement = document.querySelector('nav')
+const ulElement = document.createElement('ul')
+const liElement = document.createElement('li')
+const aElement = document.createElement('a')
+
+liElement.appendChild(aElement)
+ulElement.appendChild(liElement)
+navElement.appendChild(ulElement)
+
+aElement.innerText = 'start'
+aElement.setAttribute('href', '/')
+
+const liElement1 = liElement.cloneNode(true)
+const aElement1 = liElement1.querySelector('a')
+aElement1.innerText = 'galeria'
+aElement1.href = '/gallery'
+
+ulElement.appendChild(liElement1)
+
+const liElement2 = liElement.cloneNode(true)
+const aElement2 = liElement2.querySelector('a')
+aElement2.innerText = 'kontakt'
+aElement2.href = '/contact'
+
+ulElement.appendChild(liElement2)
+
+
+
 // struktura do wykorzystania w pętli
 const menuItems = [
     {text: 'start', url: '/'},
