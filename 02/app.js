bd767b4761327d245a49b6e6e2ae9c9f375fdd28
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-04-19 22:33:52 +0200

    Task 02 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const elList = document.querySelectorAll('[data-url]')
+console.log(elList) 
+
+
+elList.forEach(function(el){
+ const att = el.getAttribute('data-url')
+
+ el.setAttribute('href', att)
+})
