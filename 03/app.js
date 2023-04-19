20948393aa720ed6d0b752ee1113e170b30ef9b7
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-04-19 23:09:33 +0200

    Task 03 done

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,33 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parentButton = document.querySelector('.parent-for-button')
+const newButton = document.createElement('button')
+if(parentButton){
+    parentButton.appendChild(newButton)
+}
+
+for (const key in buttonSettings.attr) {
+    const value = buttonSettings.attr[key]
+    newButton[key] = value
+}
+
+for (const key in buttonSettings.css) {
+    const value = buttonSettings.css[key]
+    newButton.style[key] = value
+}
+
+newButton.innerText = buttonSettings.text
+
+
+// newButton.title = buttonSettings.attr.className
+// newButton.className = buttonSettings.attr.className
+// newButton.classList.add(buttonSettings.attr.className)
+// newButton.innerText = buttonSettings.text
+// newButton.style.border = buttonSettings.css.border
+// newButton.style.padding = buttonSettings.css.padding
+// newButton.style.color = buttonSettings.css.color
+// const btnAttr = buttonSettings.attr.title
+// newButton.getAttribute(btnAttr)
+// newButton.setAttribute('title', btnAttr)
