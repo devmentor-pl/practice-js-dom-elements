ad946f0259d2f7ec2d04ac602a1d9177a744bbe7
MS <maria.siedlik@gmail.com>
2023-04-23 13:51:04 +0200

    rozwiazanie

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const elementsWithComments = document.getElementsByClassName('comments__item'&&`comments__item--newest`);
+
+for(let i=0;i<elementsWithComments.length;i++){
+   console.log(elementsWithComments[i].hasAttribute('data-info'));
+}
+
+
+
--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+const links=document.querySelectorAll('a');
+let linksDataURL;
+
+links.forEach(function(element){
+
+   linksDataURL=element.getAttribute('data-url');
+
+   if(element.hasAttribute('data-url')){
+      element.setAttribute('href', linksDataURL);
+      console.log(element.getAttribute('href'));  
+   }
+
+})
