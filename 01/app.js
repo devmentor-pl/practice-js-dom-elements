16838075e64e73a61015f8013b175ba5c78bff6c
anetasobanska <sannettka@gmail.com>
2023-07-02 23:12:51 +0200

    finish 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const commentsItem = document.querySelectorAll('.comments__item.comments__item--newest');
+console.log('commentsItem.length', commentsItem.length);
+
+if(commentsItem) {
+    const dataInfo = commentsItem[0].querySelectorAll('[data-info]');
+    console.log('dataInfo', dataInfo);
+    console.log('dataInfo.length', dataInfo.length);
+}
