9837a523900ebf7453f7a5ad3dff061cea9597d3
anetasobanska <sannettka@gmail.com>
2023-07-03 07:14:24 +0200

    finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const linksList = document.querySelectorAll('a');
+const linksListHasUrl = [...linksList].filter(url => url.hasAttribute('data-url'));
+if(linksListHasUrl) {
+    linksListHasUrl.forEach(url => {
+        const href = url.getAttribute('data-url');
+        url.setAttribute('href', href);
+    });
+};
