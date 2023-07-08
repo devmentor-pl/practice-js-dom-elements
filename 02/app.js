3f1e2201b1b2b9f8742770f6e4ba30b8a38eb821
Wojciech Flasinski <wojciech.flasinski@gmail.com>
2023-07-08 08:18:32 +0200

    ex. 2 finish

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+
+const linkItems = document.querySelectorAll('[data-url]');
+
+if (linkItems) {
+  linkItems.forEach (function (element) {
+    attrib = element.getAttribute('data-url');
+    element.setAttribute('href', attrib);
+  });
+}
+
+
+
+
