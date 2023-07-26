0ef6abbf8c925215f57b8f9bf7584bea27aba9e9
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-26 22:51:47 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
 console.log('DOM');
+
+const listEl = document.querySelectorAll('li > [data-url]');
+
+if (listEl) {   
+  const first = listEl[0].dataset.url;
+  const second = listEl[1].dataset.url;
+  const third = listEl[2].dataset.url;
+  
+  listEl[0].setAttribute('href', first);
+  listEl[1].setAttribute('href', second);
+  listEl[2].setAttribute('href', third)
+}
+
