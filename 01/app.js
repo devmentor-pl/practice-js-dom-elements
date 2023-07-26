3bbc5ca785c5144af2d621315c9440cd2b24f6b4
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-26 18:45:39 +0200

    task 1 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const commentList = document.querySelector('.comments__item.comments__item--newest');
+
+if (commentList) {
+    const dataSpan = commentList.querySelectorAll('[data-info]');
+    console.log(`ilość elementów z atrybutem dataset = ${dataSpan.length}`)
+}
