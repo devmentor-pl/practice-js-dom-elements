67830eb295d7f5d2e7079bc8a196c0efce3a1c44
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-27 21:40:33 +0200

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,20 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parent = document.querySelector('.parent-for-button');
+
+
+
+if (parent) {
+    const btn = document.createElement('button');   
+    parent.appendChild(btn);
+
+    for(const key in buttonSettings.attr) {
+        btn.setAttribute(key, buttonSettings.attr[key])
+    }
+    for (const key in buttonSettings.css) {
+        btn.style[key] = buttonSettings.css[key];
+    }
+    btn.textContent = buttonSettings.text
+}
