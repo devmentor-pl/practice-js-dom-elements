99bda47c1bb49bfe6959ae28500a9fdeed7a9fc5
Marcin <xcinek1337@wp.pl>
2023-07-29 17:11:22 +0200

    01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,6 @@
 console.log('DOM');
+const someElem = document.querySelector('.comments__item');
+console.log(someElem);
+const dataInfoElems = someElem.querySelectorAll('[data-info]');
+console.log(dataInfoElems);
+console.log('ilość elementów: ', dataInfoElems.length);
