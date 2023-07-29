549bb7c409e04bd3c209f64ac50a6b6e6931d114
Marcin <xcinek1337@wp.pl>
2023-07-29 17:53:49 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+const linkList = document.querySelectorAll('[data-url]');
+
+linkList.forEach(el => {
+	const attribute = el.getAttribute('data-url');
+	el.setAttribute('href', attribute);
+});
