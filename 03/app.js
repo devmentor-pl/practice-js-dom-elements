f64be2f20f7e0377dc360a313d4a24ede8fd290a
Marcin <xcinek1337@wp.pl>
2023-07-29 18:44:48 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,29 @@
-console.log('DOM');
+// console.log('DOM');
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+	attr: {
+		className: 'btn',
+		title: 'super button',
+	},
+	css: {
+		border: '1px solid #336699',
+		padding: '5px 20px',
+		color: '#444',
+	},
+	text: 'Click me!',
+};
+
+const sectionElem = document.querySelector('.parent-for-button');
+const btnElement = document.createElement('button');
+
+for (const attrKeys in buttonSettings.attr) {
+	btnElement.setAttribute(attrKeys, buttonSettings.attr[attrKeys]);
 }
+for (const cssKeys in buttonSettings.css) {
+	btnElement.style[cssKeys] = buttonSettings.css[cssKeys];
+}
+for (const textKeys in buttonSettings.text) {
+	btnElement.textContent = buttonSettings.text;
+}
+
+sectionElem.appendChild(btnElement);
