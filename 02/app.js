6a1543ebd66c0052e8ae3bd730d6643340d284f9
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-07-31 21:42:54 +0200

    add forEach to listEl && set attribute to item

--- 02/app.js
+++ 02/app.js
@@ -2,13 +2,8 @@ console.log('DOM');
 
 const listEl = document.querySelectorAll('li > [data-url]');
 
-if (listEl) {   
-  const first = listEl[0].dataset.url;
-  const second = listEl[1].dataset.url;
-  const third = listEl[2].dataset.url;
-  
-  listEl[0].setAttribute('href', first);
-  listEl[1].setAttribute('href', second);
-  listEl[2].setAttribute('href', third)
-}
+  listEl.forEach(function(item) {
+    const dataValue = item.dataset.url;
+    item.setAttribute('href', dataValue)
+  })
 
