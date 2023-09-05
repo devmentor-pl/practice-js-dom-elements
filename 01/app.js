558d94b97faeadb93069df963f82464768c89754
Piotr <you@example.com>
2023-09-05 22:39:48 +0200

    add task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,6 @@
 console.log('DOM');
+
+
+const element = document.querySelector(".comments__item.comments__item--newest");
+const elementTwo = element.querySelectorAll('[data-info]');
+console.log(`Liczba elementów z atrybutem data-info: ${elementTwo.length}`);
