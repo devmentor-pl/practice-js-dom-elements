d2877f457c8dace7fdbe05af5ff7a7d91745c525
Piotr <you@example.com>
2023-09-10 14:50:41 +0200

    add task 5

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,43 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+// dodanie przycisku 
+const deleteButton = document.createElement('button')
+deleteButton.textContent = "usuń z koszyka"
+curr.parentNode.insertBefore(deleteButton, curr.nextSibling);
+
+// Do wszystkich elementów, które są rodzeństwem elementu o klasie .js-curr, dodaj klasę .siblings (wykorzystaj pętlę).
+
+const newClassSiblings = curr.parentNode.children
+for ( let i =0; i< newClassSiblings.length; i++ ){
+    if (newClassSiblings[i] !== curr) {
+        newClassSiblings[i].classList.add("siblings")
+    }
+}
+
+// Do następnego elementu o klasie .article, który występuje zaraz po rodzicu elementu o klasie .js-curr, dodaj atrybut title o wartości nextElementSibling.
+
+const art = curr.closest('article') 
+const newArticle = art.nextElementSibling
+if (newArticle && newArticle.classList.contains("article")) {
+    newArticle.setAttribute('title', 'nextElementSibling')
+}
+
+
+// Do ostatniego artykułu dodaj dodatkowy paragraf i umieść go przed znacznikiem <button/>.
+
+const endArticle = document.querySelector('.article:last-child')
+const addParagraf = document.createElement('p')
+addParagraf.textContent ="Dodanie nowego paragrafu aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+endArticle.insertBefore(addParagraf, endArticle.querySelector('button'));
+
+
+// Na początku listy dodaj kolejny artykuł, który będzie miał tę samą strukturę, co pozostałe artykuły.
+
+const newFirstArticle = document.querySelector('.article:first-child') 
+const topArticle = newFirstArticle.cloneNode(true);
+
+const articlesAdd = document.querySelector('.articles');
+const firstArticle = articlesAdd.querySelector('.article:first-child');
+articlesAdd.insertBefore(topArticle, firstArticle);
