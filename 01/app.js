8f8ef8f71dd4865b6ac0c7d26f9adbf6942d0dc6
Maciej Necka <macieknecka@gmail.com>
2023-09-21 22:09:26 +0200

    Task done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
-console.log('DOM');
+'use strict';
+const liElement = document.querySelector(
+  '.comments__item.comments__item--newest'
+);
+if (liElement) {
+  const dataElement = liElement.querySelectorAll('[data-info]');
+  console.log(dataElement.length);
+}
