f035ae92bf7657208d44441f95ace8401adfc754
Maciej Necka <macieknecka@gmail.com>
2023-09-21 22:22:30 +0200

    Task done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
-console.log('DOM');
+const dataElements = document.querySelectorAll('[data-url]');
+if (dataElements) {
+  for (let i = 0; i < dataElements.length; i++) {
+    const dataAttribute = dataElements[i].getAttribute('data-url');
+    console.log(dataAttribute);
+    dataElements[i].setAttribute('href', dataAttribute);
+  }
+}
