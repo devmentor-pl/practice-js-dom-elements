b9d08930d96560b12febe67f7012c2b76a7bb6e0
Maciej Necka <macieknecka@gmail.com>
2023-09-24 23:24:35 +0200

    if statement removed

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,6 @@
 const dataElements = document.querySelectorAll('[data-url]');
-if (dataElements) {
-  for (let i = 0; i < dataElements.length; i++) {
-    const dataAttribute = dataElements[i].getAttribute('data-url');
-    console.log(dataAttribute);
-    dataElements[i].setAttribute('href', dataAttribute);
-  }
+for (let i = 0; i < dataElements.length; i++) {
+  const dataAttribute = dataElements[i].getAttribute('data-url');
+  console.log(dataAttribute);
+  dataElements[i].setAttribute('href', dataAttribute);
 }
