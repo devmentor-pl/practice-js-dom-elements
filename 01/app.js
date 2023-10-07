336614cf1dac36b8cf14b5e45415b82a36715f54
Kaczupinko <kaczupinko@gmail.com>
2023-10-07 15:29:16 +0100

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+//Wyszukaj element, który ma dwie klasy: `.comments__item` oraz `.comments__item--newest`.
+//Następnie poszukaj w nim elementów, które posiadają atrybut `data-info`.
+//Wyświetl w konsoli, ile takich elementów zostało znalezionych.
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+if(element) {
+    const dataInfo = element.querySelectorAll('[data-info]');
+    console.log(dataInfo.length);
+}
