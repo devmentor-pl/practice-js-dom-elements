3bfe2be38bf1364832990bf2895004b2b974878a
Kaczupinko <kaczupinko@gmail.com>
2023-10-09 12:27:04 +0100

    Refactor 01 task

--- 01/app.js
+++ 01/app.js
@@ -1,10 +1,7 @@
 console.log('DOM');
-//Wyszukaj element, który ma dwie klasy: `.comments__item` oraz `.comments__item--newest`.
-//Następnie poszukaj w nim elementów, które posiadają atrybut `data-info`.
-//Wyświetl w konsoli, ile takich elementów zostało znalezionych.
 
 const element = document.querySelector('.comments__item.comments__item--newest');
-if(element) {
+if (element) {
     const dataInfo = element.querySelectorAll('[data-info]');
     console.log(dataInfo.length);
 }
