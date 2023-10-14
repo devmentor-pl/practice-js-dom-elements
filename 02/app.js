7915a1f62bf4c83147cc3ff96ad438c6326fd055
Marcin Chechelski <marcin.chechelski@op.pl>
2023-10-14 22:03:08 +0200

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,6 @@
-console.log('DOM');
+console.log("DOM");
+const links = document.querySelectorAll(`a[data-url]`);
+for (let i = 0; i < links.length; i++) {
+    const url = links[i].getAttribute("data-url");
+    links[i].setAttribute("href", url);
+}
