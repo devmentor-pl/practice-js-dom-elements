534cb52fa6abb268445475d5530a4053a43ab663
Marcin Chechelski <marcin.chechelski@op.pl>
2023-10-14 21:40:04 +0200

    task done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
-console.log('DOM');
+console.log("DOM");
+
+const elements = document.querySelector(
+    ".comments__item.comments__item--newest"
+);
+
+if (elements) {
+    const data = document.querySelectorAll("[data-info]");
+
+    console.log("Zostało znalezionych " + data.length + " elementów");
+}
