9ab9c62d220b2a6a0d30ff90187ea37c91fdf3e6
Kasia <k.kasia912@gmail.com>
2023-11-23 13:25:13 +0100

    complete task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,14 @@
 console.log('DOM');
+
+const comentsNewest = document.querySelector(
+  '.comments__item.comments__item--newest'
+);
+if (comentsNewest) {
+  console.log(comentsNewest);
+  const commentLink = comentsNewest.querySelectorAll('[data-info]');
+  if (commentLink) {
+    console.log(
+      `Zostały znalezione ${commentLink.length} elementy z atrybutem data-info w rodzicu commentsNewest`
+    );
+  }
+}
