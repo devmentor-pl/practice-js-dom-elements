fa8f6d2b797600256df9eaa4874359677a42b005
Kasia <k.kasia912@gmail.com>
2023-11-24 15:09:46 +0100

    complete task 05

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,32 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+// Task 1
+const removeBtn = document.createElement('button');
+removeBtn.innerText = 'Usuń z koszyka';
+curr.parentElement.appendChild(removeBtn);
+
+// Task 2
+const currSiblings = [...curr.parentElement.children];
+currSiblings.forEach((el) => {
+  el.classList.add('siblings');
+});
+// Task 3
+curr.parentElement.nextElementSibling.setAttribute(
+  'title',
+  'nextElementSibling'
+);
+
+// Task 4
+const newParagraph = document.createElement('p');
+newParagraph.innerText = 'lorem Ipsum';
+const lastArticleEl = curr.parentElement.nextElementSibling.nextElementSibling;
+const lastArticleBtn =
+  lastArticleEl.children[lastArticleEl.children.length - 1];
+lastArticleEl.insertBefore(newParagraph, lastArticleBtn);
+
+// Task 5
+const sectionEl = curr.parentElement.parentElement;
+const newArticle = curr.parentElement.cloneNode(true);
+sectionEl.insertBefore(newArticle, sectionEl.firstChild);
