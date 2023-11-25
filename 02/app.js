acbc97007c587e1eb4aefdfeca4b4fba0b0d28dd
Kasia <k.kasia912@gmail.com>
2023-11-25 12:13:52 +0100

    remove if statment task 02

--- 02/app.js
+++ 02/app.js
@@ -1,12 +1,10 @@
 console.log('DOM');
 
 const urlList = document.querySelectorAll('a[data-url]');
-if (urlList.length > 0) {
-  urlList.forEach((item) => {
-    if (item.hasAttribute('data-url')) {
-      const urlValue = item.dataset.url;
-      item.setAttribute('href', urlValue);
-      console.log(item);
-    }
-  });
-}
+urlList.forEach((item) => {
+  if (item.hasAttribute('data-url')) {
+    const urlValue = item.dataset.url;
+    item.setAttribute('href', urlValue);
+    console.log(item);
+  }
+});
