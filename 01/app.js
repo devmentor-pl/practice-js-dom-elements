93a5734482c4ad6b63fe3485f50730f80dbdd51f
Natalia Gron <nanusza@gmail.com>
2023-12-03 20:07:54 +0100

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,7 @@
 console.log('DOM');
+const element = document.querySelector('.comments__item.comments__item--newest');
+
+if(element) {
+    const elWithAtt = element.querySelectorAll('[data-info]');
+    console.log(elWithAtt.length);
+}
