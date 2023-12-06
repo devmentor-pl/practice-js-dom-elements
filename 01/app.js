e18bd9eacfdb8e9f90353f53a3855b793ceeb133
Kamil Lewandowski <kamlew555@gmail.com>
2023-12-06 19:22:02 +0100

    exercise done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+const itemEls = document.querySelectorAll('.comments__item.comments__item--newest');
+
+let count = 0;
+itemEls.forEach(el => {
+   const newEls = el.querySelectorAll('[data-info]')
+   count += newEls.length
+   console.log(`Znalezono: ${count} elementy z atrybutem [data-info]`);
+})
+
+
+
+
+
