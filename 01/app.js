62cfac6104bc7ffc8384613d8e100d2ddf7fedc9
Lukasz <l.bdnski@gmail.com>
2023-12-08 14:44:20 +0100

    add task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const commentsContainer = document.querySelector('.comments__item.comments__item--newest')
+console.log(commentsContainer)
+
+if (commentsContainer) {
+    const dataInfo = commentsContainer.querySelectorAll('[data-info]')
+    console.log(dataInfo.length)
+}
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,25 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newButton = document.createElement('button')
+newButton.textContent = 'Usuń z koszyka'
+const domArticle = curr.parentElement
+domArticle.append(newButton)
+
+const domChild = domArticle.children
+for (let i=0;i<domChild.length;i++)
+    if (!domChild[i].className.includes('js-curr'))
+        domChild[i].classList.add('siblings')
+   
+const eventsArticle = domArticle.nextElementSibling
+eventsArticle.title = 'nextElementSibling'
+
+const formsArticle = eventsArticle.nextElementSibling
+const buttonForm = formsArticle.lastElementChild
+const newPar = document.createElement('p')
+formsArticle.insertBefore(newPar,buttonForm)     
+
+const articles = formsArticle.parentElement
+const articleClone = formsArticle.cloneNode(true) 
+articles.insertBefore(articleClone,domArticle)
