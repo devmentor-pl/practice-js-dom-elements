61cca416c2136c64903b16aaab78537be6082dcf
Kamil Lewandowski <kamlew555@gmail.com>
2023-12-10 11:06:21 +0100

    task done with forEach method

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,22 @@
-console.log('DOM');
+console.log("DOM");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+	{ text: "start", url: "/" },
+	{ text: "galeria", url: "/gallery" },
+	{ text: "kontakt", url: "/contact" },
 ];
+
+
+const navElement = document.querySelector('nav')
+menuItems.forEach(item => {
+	const liElements = document.createElement('li');
+	const linkElements = document.createElement('a')
+
+	linkElements.href = item.url
+	linkElements.textContent = item.text
+
+	liElements.appendChild(linkElements)
+	navElement.appendChild(liElements)
+})
+
