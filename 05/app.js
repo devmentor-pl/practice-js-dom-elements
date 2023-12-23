1b9c8d4da66714158f33d5b59173002ad282a4e0
Dawidtk <dawidtk@gmail.com>
2023-12-23 14:06:44 +0100

    done

--- 05/app.js
+++ 05/app.js
@@ -2,12 +2,12 @@ console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
 
-//1. Utworzenie i dodanie nowego przycisku jako ostatnie dziecko rodzica elementu curr 
+// Utworzenie i dodanie nowego przycisku jako ostatnie dziecko rodzica elementu curr 
 const newButton = document.createElement('button');
 newButton.textContent = 'Usuń z koszyka';
 curr.parentNode.appendChild(newButton);
 
-/*2. Dodanie klasy .siblings do rodzeństwa. Pobiera wszystkie dzieci rodzica elementu curr.
+/* Dodanie klasy .siblings do rodzeństwa. Pobiera wszystkie dzieci rodzica elementu curr.
 Przechodzi przez każde dziecko dodając klasę .siblings ,które nie są równorzędne elementowi curr. */
 
 const siblings = curr.parentNode.children;
@@ -17,7 +17,6 @@ for (let sibling of siblings) {
     }
 }
 
-//3. 
 const nextArticle = curr.parentNode.nextElementSibling.querySelector('.article');
 if (nextArticle) {
     nextArticle.setAttribute('title', 'nextElementSibling');
@@ -27,4 +26,15 @@ if (lastArticle) {
     const newParagraph = document.createElement('p');
     newParagraph.textContent = 'Dodatkowy paragraf przed przyciskiem.';
     lastArticle.insertBefore(newParagraph, lastArticle,querySelector('button'));
+}
+
+// Dodanie nowego artykułu na początku listy 
+
+const firstArticle = document.querySelector('.articles__item:first-child');
+if (firstArticle) {
+    const newArticle = firstArticle.cloneNode(true);
+    newArticle.querySelector('.article__title').textContent = 'Nowy artykuł';
+    newArticle.querySelector('.article__description').textContent = 'Opis nowego artykułu.';
+    newArticle.querySelector('.js-curr').textContent = 'Nowy przycisk';
+    firstArticle.before(newArticle);
 }
