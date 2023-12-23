b40c0608293cdfa3412d30f0083193532bccb0c7
Dawidtk <dawidtk@gmail.com>
2023-12-23 09:43:01 +0100

    create and add button

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,8 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+//1. Utworzenie i dodanie nowego przycisku
+const newButton = document.createElement('button');
+newButton.textContent = 'Usuń z koszyka';
+curr.parentNode.appendChild(newButton);
