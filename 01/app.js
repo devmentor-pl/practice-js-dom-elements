c91a3357d2157c664091b2becd437804522d4987
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-29 21:15:37 +0100

    Implement finding and counting elements with specific classes and data attributes

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const className = document.querySelector('.comments__item.comments__item--newest');
+let dataInfo;
+if(className !== null) {
+    dataInfo = className.dataset.info;
+};
+
+const elementsWithDataInfo = document.querySelectorAll('[data-info');
+console.log(`Ilość znalezionych elementów data-info wynosi ${elementsWithDataInfo.length}`)
