45aad62482fe93ae776fd28c73ce8e0bede2861b
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-29 21:59:53 +0100

    Replace data-url attributes with href attributes for all links

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
 console.log('DOM');
+
+const linksData = document.querySelectorAll('a[data-url]');
+const dataUrls = [];
+
+linksData.forEach(function(link){
+    const urlValue = link.getAttribute('data-url');
+    dataUrls.push(urlValue);
+
+    link.setAttribute('href', urlValue);
+});
+
+
+
