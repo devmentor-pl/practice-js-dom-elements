e3d7400fd655892c70ca8099588a0aebe2df667a
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-31 00:01:58 +0100

    Add new button to the DOM structure

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,7 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const button = document.createElement('button');
+button.textContent = 'usuń z koszyka';
+curr.appendChild(button);
