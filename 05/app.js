5da3eccee921aecba15aec7b79ad047af177c9c7
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-31 00:41:38 +0100

    Add 'siblings' class to all siblings of the element with the class 'js-curr'

--- 05/app.js
+++ 05/app.js
@@ -5,3 +5,13 @@ const curr = document.querySelector('.js-curr');
 const button = document.createElement('button');
 button.textContent = 'usuń z koszyka';
 curr.appendChild(button);
+
+if (curr) {
+    const siblings = Array.from(curr.parentNode.children);
+
+    for (const sibling of siblings) {
+        if (sibling !== curr) {
+            sibling.classList.add('siblings');
+        }
+    }
+}
