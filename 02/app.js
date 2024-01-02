4192d11e7efaa95c464f9ca8892e0491dbd9f363
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-02 19:44:00 +0100

    Remove unnecessary data collection, directly assign 'data-url' values to 'href' attributes.

--- 02/app.js
+++ 02/app.js
@@ -1,14 +1,9 @@
 console.log('DOM');
 
 const linksData = document.querySelectorAll('a[data-url]');
-const dataUrls = [];
 
 linksData.forEach(function(link){
     const urlValue = link.getAttribute('data-url');
-    dataUrls.push(urlValue);
 
     link.setAttribute('href', urlValue);
 });
-
-
-
