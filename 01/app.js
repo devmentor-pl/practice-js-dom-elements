65175c0463c90ca8d4e6f57314016bddce55716b
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-02 19:37:27 +0100

    Optimize search for elements with data-info attribute.

--- 01/app.js
+++ 01/app.js
@@ -1,10 +1,8 @@
 console.log('DOM');
 
 const className = document.querySelector('.comments__item.comments__item--newest');
-let dataInfo;
-if(className !== null) {
-    dataInfo = className.dataset.info;
-};
 
-const elementsWithDataInfo = document.querySelectorAll('[data-info');
-console.log(`Ilość znalezionych elementów data-info wynosi ${elementsWithDataInfo.length}`)
+if(className !== null) {
+    const elementsWithDataInfo = className.querySelectorAll('[data-info]');
+    console.log(`Ilość znalezionych elementów data-info wynosi ${elementsWithDataInfo.length}`)
+};
