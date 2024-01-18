eda0b8c04a43e2004ba9111010cc972d6a4a4e38
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-18 21:09:06 +0100

    Finish 03

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,14 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+const buttonParent = document.querySelector('.parent-for-button');
+const button = document.createElement('button');
+
+button.setAttribute('class', buttonSettings.attr.className);
+button.setAttribute('title', buttonSettings.attr.title);
+button.innerText = buttonSettings.text;
+for (const key in buttonSettings.css) {
+  button.style[key] = buttonSettings.css[key];
+}
+
+buttonParent.appendChild(button);
