7726c8b3b5e16ce5ca328399a05eb12c8a26c1ad
Marcin Wabinski <wabinskim@gmail.com>
2024-02-13 03:37:28 +0100

    Task 04 finished

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,20 @@
-console.log('DOM');
-
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: "start", url: "/" },
+  { text: "galeria", url: "/gallery" },
+  { text: "kontakt", url: "/contact" },
 ];
+
+const navEl = document.querySelector("nav");
+if (navEl) {
+  const ulEl = document.createElement("ul");
+  navEl.appendChild(ulEl);
+  menuItems.forEach(function (element) {
+    const liEl = document.createElement("li");
+    ulEl.appendChild(liEl);
+    const aEl = document.createElement("a");
+    liEl.appendChild(aEl);
+    aEl.setAttribute("href", element.url);
+    aEl.innerText = element.text;
+  });
+}
