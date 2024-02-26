6dc6f74618dd2cf931586d13d6a6badd3d268383
Paweł Suder <pawel.suder94@gmail.com>
2024-02-26 12:59:28 +0100

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const searchAllLinks = document.querySelectorAll('[data-url]')
+console.log(searchAllLinks)
+
+searchAllLinks.forEach(function (element) {
+    const attr = element.getAttribute('data-url')
+    element.setAttribute('href', attr)
+})
