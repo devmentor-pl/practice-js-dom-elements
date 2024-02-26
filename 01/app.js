e38f40decf02c3ea149094c9f700a2cea916493f
Paweł Suder <pawel.suder94@gmail.com>
2024-02-26 12:35:34 +0100

    task 1 done

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+
+if (element) {
+    const howManyAttributes = element.querySelectorAll('[data-info]')
+    console.log('Liczba elementów posiadających określony atrybut:', howManyAttributes.length)
+} else {
+    console.error('Brak takiego elementu');
+}
+
