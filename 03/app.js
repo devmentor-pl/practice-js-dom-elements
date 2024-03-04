fe4a255f72327d48c3d58474c43de1b616d3158c
Paweł Suder <pawel.suder94@gmail.com>
2024-03-04 11:24:31 +0100

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,19 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parentElement = document.querySelector('.parent-for-button')
+const newButton = document.createElement('button')
+parentElement.appendChild(newButton)
+
+for (const key in buttonSettings.attr) {
+    const value = buttonSettings.attr[key]
+    newButton[key] = value
+}
+
+for (const key in buttonSettings.css) {
+    const value = buttonSettings.css[key]
+    newButton.style[key] = value
+}
+
+newButton.innerText = buttonSettings.text
