ba8f33001eaddbcb119fcdb9478a0b115bfd6f33
Rafal <rafk528@gmail.com>
2024-03-12 21:54:49 +0000

    links

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,12 @@
-console.log('DOM');
+const links = document.querySelectorAll("a[data-url]")
+
+links.forEach((link) => {
+  const url = link.getAttribute("data-url")
+
+  if (url) {
+    link.setAttribute("href", url)
+    link.setAttribute("target", "_blank")
+  } else {
+    link.removeAttribute("href")
+  }
+})
