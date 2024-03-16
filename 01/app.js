83846a7c72c5f75104eb38b5c2b78761cb24539a
Rafal <rafk528@gmail.com>
2024-03-16 18:24:11 +0000

    elements

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const newestCommentItem = document.querySelector(
+  ".comments__item.comments__item--newest"
+)
+
+const infoElements = newestCommentItem.querySelectorAll("[data-info]")
+
+console.log(infoElements.length)
