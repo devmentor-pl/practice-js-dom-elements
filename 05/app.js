ac6b3742929afd18908a9073886ac67183bfc28f
Rafal <rafk528@gmail.com>
2024-03-17 13:31:17 +0000

    articles

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,30 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const button = document.createElement("button")
+button.textContent = "usuń z koszyka"
+button.style.marginLeft = "5px"
+curr.parentNode.insertBefore(button, curr.nextSibling)
+
+let sibiling = curr.parentNode.firstChild
+while (sibiling) {
+  if (sibiling.nodeType === 1 && sibiling !== curr) {
+    sibiling.classList.add("sibiling")
+  }
+  sibiling = sibiling.nextSibling
+}
+
+const nextArticle = curr.parentNode.nextElementSibling
+if (nextArticle && nextArticle.classList.contains("article")) {
+  nextArticle.setAttribute("title", "nextElementSibiling")
+}
+
+const lastArticle = document.querySelector(".articles").lastElementChild
+const paragraph = document.createElement("p")
+paragraph.textContent = "Dodatkowy paragraf"
+lastArticle.insertBefore(paragraph, lastArticle.querySelector("button"))
+
+const firstArticle = document.querySelector(".articles").firstElementChild
+const article = firstArticle.cloneNode(true)
+document.querySelector(".articles").insertBefore(article, firstArticle)
