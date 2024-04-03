6f594d8c5fc2f99e188c0357aa9a96c97014dd01
Jaro <jaroswidnik@interia.pl>
2024-04-03 12:09:37 +0200

    Finished first part of Ex4 in DOM Elements

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,37 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const navbar = document.querySelector('nav')
+
+//Tworzenie li
+const liItem1 = document.createElement('li');
+const aItem = document.createElement('a');
+aItem.setAttribute("href", '/');
+aItem.innerText = 'home';
+liItem1.appendChild(aItem);
+
+const liItem2 = document.createElement('li');
+const aItem2 = document.createElement('a');
+aItem2.setAttribute("href", '/gallery');
+aItem2.innerText = 'gallery';
+liItem2.appendChild(aItem2);
+
+const liItem3 = document.createElement('li');
+const aItem3 = document.createElement('a');
+aItem3.setAttribute("href", '/contact');
+aItem3.innerText = 'contact';
+liItem3.appendChild(aItem3);
+
+
+// Tworzenie ul listy
+const ulList = document.createElement('ul')
+
+ulList.appendChild(liItem1)
+ulList.appendChild(liItem2)
+ulList.appendChild(liItem3)
+
+console.log(ulList);
+console.log(navbar);
+
+navbar.appendChild(ulList)
