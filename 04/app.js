868749f37112aa5850aace0e2b10a707045acc95
Jaro <jaroswidnik@interia.pl>
2024-04-03 13:25:57 +0200

    Made some changes in ex4 in DOM Elements

--- 04/app.js
+++ 04/app.js
@@ -1,42 +1,67 @@
-console.log('DOM');
+// console.log('DOM');
+
+// const navbar = document.querySelector("nav");
+
+// //Tworzenie li
+// const liItem1 = document.createElement("li");
+// const aItem = document.createElement("a");
+// aItem.setAttribute("href", "/");
+// aItem.innerText = "home";
+// liItem1.appendChild(aItem);
+
+// const liItem2 = document.createElement("li");
+// const aItem2 = document.createElement("a");
+// aItem2.setAttribute("href", "/gallery");
+// aItem2.innerText = "gallery";
+// liItem2.appendChild(aItem2);
+
+// const liItem3 = document.createElement("li");
+// const aItem3 = document.createElement("a");
+// aItem3.setAttribute("href", "/contact");
+// aItem3.innerText = "contact";
+// liItem3.appendChild(aItem3);
+
+// // Tworzenie ul listy
+// const ulList = document.createElement("ul");
+
+// ulList.appendChild(liItem1);
+// ulList.appendChild(liItem2);
+// ulList.appendChild(liItem3);
+
+// console.log(ulList);
+// console.log(navbar);
+
+// navbar.appendChild(ulList);
+
+// ZA POMOCĄ PĘTLI
+
+const navbar = document.querySelector("nav");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+	{ text: "start", url: "/" },
+	{ text: "galeria", url: "/gallery" },
+	{ text: "kontakt", url: "/contact" },
 ];
 
-const navbar = document.querySelector('nav')
+const ulList = document.createElement("ul")
+const liItem = document.createElement("li");
+const aItem = document.createElement("a");
 
-//Tworzenie li
-const liItem1 = document.createElement('li');
-const aItem = document.createElement('a');
-aItem.setAttribute("href", '/');
-aItem.innerText = 'home';
-liItem1.appendChild(aItem);
+menuItems.forEach(function (item) {
+	console.log(item);
 
-const liItem2 = document.createElement('li');
-const aItem2 = document.createElement('a');
-aItem2.setAttribute("href", '/gallery');
-aItem2.innerText = 'gallery';
-liItem2.appendChild(aItem2);
+	for (let key in item) {
+		if (key === "text") {
+			aItem.innerText = item[key];
+		} else if (key === "url") {
+			aItem.setAttribute("href", item[key]);
+		}
+	}
+    liItem.appendChild(aItem);
+    ulList.appendChild(liItem)
+    // console.log("Klucz:", key);
+    // console.log("Wartość:", item[key]);
+    navbar.appendChild(ulList)
+});
 
-const liItem3 = document.createElement('li');
-const aItem3 = document.createElement('a');
-aItem3.setAttribute("href", '/contact');
-aItem3.innerText = 'contact';
-liItem3.appendChild(aItem3);
-
-
-// Tworzenie ul listy
-const ulList = document.createElement('ul')
-
-ulList.appendChild(liItem1)
-ulList.appendChild(liItem2)
-ulList.appendChild(liItem3)
-
-console.log(ulList);
-console.log(navbar);
-
-navbar.appendChild(ulList)
