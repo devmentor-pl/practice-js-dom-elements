c2017ed15f0391c93c1bd86b20324bfd2ba298f7
Jaro <jaroswidnik@interia.pl>
2024-04-03 09:13:23 +0200

    Finished ex1 in DOM Elements

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,17 @@
-console.log('DOM');
+// console.log('DOM');
+const pickedElement = document.querySelector('.comments__item.comments__item--newest')
+
+console.log(pickedElement);
+
+const dataInfoAtr = pickedElement.querySelectorAll('[data-info]')
+
+console.log(dataInfoAtr);
+console.log("Ilość elementów posiadających atrybut data-info:", dataInfoAtr.length);
+
+
+
+
+
+
+
+
