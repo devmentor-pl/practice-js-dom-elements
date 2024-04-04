8867afc039fb4289f7de26829bc5cb96b2beb7e4
Jaro <jaroswidnik@interia.pl>
2024-04-04 11:58:06 +0200

    Finished part 3 in ex4 in DOM Elements

--- 05/app.js
+++ 05/app.js
@@ -2,6 +2,8 @@ console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
 
+// NR 1
+
 const newButton = document.createElement('button');
 newButton.innerText = 'Usuń z koszyka';
 
@@ -9,6 +11,7 @@ const article1 = curr.parentElement;
 
 article1.appendChild(newButton)
 
+// NR 2
 const art1Children = article1.children;
 console.log(art1Children);
 const arrOfArt1 = Array.from(art1Children)
@@ -18,5 +21,10 @@ arrOfArt1.forEach(function (item) {
     return item.classList.add('siblings') 
 })
 
-console.log(article1.children);
+// NR 3
+
+const article2 = article1.nextElementSibling
+// console.log(article2);
+article2.setAttribute('title', 'nextElementSibling')
+console.log(article2);
 
