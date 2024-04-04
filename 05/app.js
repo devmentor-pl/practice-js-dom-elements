d63f4b1abdf865330384a889c9230754528d6930
Jaro <jaroswidnik@interia.pl>
2024-04-04 12:51:32 +0200

    Finished part 4 in ex5 in DOM Elements

--- 05/app.js
+++ 05/app.js
@@ -1,30 +1,50 @@
-console.log('DOM');
+console.log("DOM");
 
-const curr = document.querySelector('.js-curr');
+const curr = document.querySelector(".js-curr");
 
 // NR 1
 
-const newButton = document.createElement('button');
-newButton.innerText = 'Usuń z koszyka';
+const newButton = document.createElement("button");
+newButton.innerText = "Usuń z koszyka";
 
 const article1 = curr.parentElement;
 
-article1.appendChild(newButton)
+article1.appendChild(newButton);
 
 // NR 2
 const art1Children = article1.children;
 console.log(art1Children);
-const arrOfArt1 = Array.from(art1Children)
-
+const arrOfArt1 = Array.from(art1Children);
 
 arrOfArt1.forEach(function (item) {
-    return item.classList.add('siblings') 
-})
+	return item.classList.add("siblings");
+});
 
 // NR 3
 
-const article2 = article1.nextElementSibling
+const article2 = article1.nextElementSibling;
 // console.log(article2);
-article2.setAttribute('title', 'nextElementSibling')
+article2.setAttribute("title", "nextElementSibling");
 console.log(article2);
 
+// NR 4
+
+const article3 = article2.nextElementSibling;
+console.log(typeof article3);
+
+const art3Children = article3.children;
+console.log(art3Children);
+
+const arrOfArt3Children = Array.from(art3Children);
+// console.log(arrOfArt3Children);
+// console.log(arrOfArt3Children[2]);
+
+const art3Button = arrOfArt3Children[2];
+console.log(art3Button);
+
+const art3NewPara = document.createElement("p");
+
+// Wstawienie paragrafu przed <button>
+article3.insertBefore(art3NewPara, art3Button);
+
+console.log(art3Children);
