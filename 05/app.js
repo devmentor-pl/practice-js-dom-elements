e2c4c6e63a89663e31a382084cada5fa941d5436
Jaro <jaroswidnik@interia.pl>
2024-04-04 10:36:35 +0200

    Added new button to DOM

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,12 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newButton = document.createElement('button');
+newButton.innerText = 'Usuń z koszyka';
+
+const article1 = curr.parentElement;
+
+article1.appendChild(newButton)
+
+
