ca629c680845956efbf18279585cfc20a7ea4e54
Jaro <jaroswidnik@interia.pl>
2024-04-04 10:49:44 +0200

    Added siblings class to children in ex 5 DOM Elements

--- 05/app.js
+++ 05/app.js
@@ -9,4 +9,14 @@ const article1 = curr.parentElement;
 
 article1.appendChild(newButton)
 
+const art1Children = article1.children;
+console.log(art1Children);
+const arrOfArt1 = Array.from(art1Children)
+
+
+arrOfArt1.forEach(function (item) {
+    return item.classList.add('siblings') 
+})
+
+console.log(article1.children);
 
