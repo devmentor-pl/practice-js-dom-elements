06458ec627c4fd9d11ba1f5d85e194d8ad589476
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-04-06 22:39:50 +0200

    add first task

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,17 @@
 console.log('DOM');
+
+const classElement = document.querySelector('.comments__item.comments__item--newest');
+
+if (classElement && classElement.hasChildNodes()) {
+    const children = classElement.children;
+    const newArray = Array.from(children);
+    const arr = [];
+    for (let i = 0; i < newArray.length; i++) {
+
+        if (!newArray[i].hasAttribute('data-info')) {
+            arr.push(children[i]);
+        }
+
+    }
+    console.log(arr.length);
+}
