fe39c97255d0d8e10b22fafe143bd6248554a756
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-04-08 14:08:55 +0200

    correct task 1

--- 01/app.js
+++ 01/app.js
@@ -2,14 +2,14 @@ console.log('DOM');
 
 const classElement = document.querySelector('.comments__item.comments__item--newest');
 
-if (classElement && classElement.hasChildNodes()) {
-    const children = classElement.children;
-    const newArray = Array.from(children);
-    const arr = [];
-    for (let i = 0; i < newArray.length; i++) {
+if (classElement) {
 
-        if (!newArray[i].hasAttribute('data-info')) {
-            arr.push(children[i]);
+    const allElements = classElement.querySelectorAll('p, li, span')
+
+    const arr = [];
+    for (let i = 0; i < allElements.length; i++) {
+        if (allElements[i].hasAttribute('data-info')) {
+            arr.push(allElements[i]);
         }
 
     }
