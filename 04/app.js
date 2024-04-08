5f68500803e16c32d27c168b29711004e5394386
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-04-08 19:49:29 +0200

    add second solution to the task

--- 04/app.js
+++ 04/app.js
@@ -7,18 +7,35 @@ const menuItems = [
     { text: 'kontakt', url: '/contact' },
 ];
 
+const parentNav = document.querySelector('nav')
+if (parentNav) {
 
-const ulElement = document.createElement('ul');
+    const ulElement = document.createElement('ul');
+    parentNav.appendChild(ulElement)
+
+    menuItems.forEach((value) => {
+        const liElement = document.createElement('li');
+        const aElement = document.createElement('a');
+
+        aElement.innerText = value.text;
+        aElement.setAttribute('href', value.url);
+        ulElement.appendChild(liElement)
+        liElement.appendChild(aElement)
+
+    })
+
+}
+/* const ulElement = document.createElement('ul');
 const parentNav = document.querySelector('nav')
 if (parentNav) {
     const ulElement = document.createElement('ul');
     const liElement = document.createElement('li');
     const liElement2 = liElement.cloneNode(true)
-    const liElement3 = liElement.cloneNode(true)
+     const liElement3 = liElement.cloneNode(true)
     const aElement = document.createElement('a');
     aElement.setAttribute('href', '/')
     aElement.textContent = 'start';
-
+ *
 
 
 
@@ -42,7 +59,4 @@ if (parentNav) {
     liElement.appendChild(aElement)
     liElement2.appendChild(aElement2)
     liElement3.appendChild(aElement3)
-
-
-
-}
+} */
