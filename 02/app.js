fdb597b8e2a2bb0d32c41b0e7ca1ea80b1937e07
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-04-08 13:18:16 +0200

    add task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,17 @@
 console.log('DOM');
+const linkList = document.getElementsByTagName('a')
+const sth = Array.from(linkList);
+if (sth) {
+    for (let i = 0; i < sth.length; i++) {
+        if (linkList[i].hasAttribute('data-url')) {
+            const element = linkList[i];
+            const attributeName = element.getAttribute('data-url');
+            element.setAttribute(
+                'href',
+                attributeName
+            )
+        }
+    }
+
+
+}
