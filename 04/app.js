1e5f8f08021b9e037e17276a4b9e6f8e0fa9e1cb
MarcinMorgan <morgangzit@gmail.com>
2024-04-14 23:44:28 +0200

    feat: create list of links without using menuItem structure

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,28 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const parentElement = document.querySelector('nav');
+console.log(parentElement);
+const newList = document.createElement('ul');
+parentElement.appendChild(newList);
+for (let i=0;i<3;i++) {
+    const newElement = document.createElement('li');
+    newList.appendChild(newElement);
+    const newLink = document.createElement('a');
+    switch(i) {
+        case 0:
+            newLink.innerText = 'start';
+            newLink.setAttribute('href', '/');
+            break;
+        case 1:
+            newLink.innerText = 'galeria';
+            newLink.setAttribute('href', '/gallery');
+            break;
+        case 2:
+            newLink.innerText = 'kontakt';
+            newLink.setAttribute('href', '/contact');
+            break;
+    }
+    newElement.appendChild(newLink);
+}
