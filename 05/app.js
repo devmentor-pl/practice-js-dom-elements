fe48e1c9bd0e91c0168699f1445b4934c3c04cfa
MarcinMorgan <morgangzit@gmail.com>
2024-04-15 11:26:21 +0200

    feat: declare parents of .js-curr class

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,8 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const currParent = curr.parentElement;
+const currGrandParent = currParent.parentElement;
+
+console.log(currParent)
+console.log(currGrandParent)
