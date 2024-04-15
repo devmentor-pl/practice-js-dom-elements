f7f78e981bc64de8bacb62f7bb94c0faad7cdf3b
MarcinMorgan <morgangzit@gmail.com>
2024-04-15 11:50:44 +0200

    feat: add button as sibling of .js-curr

--- 05/app.js
+++ 05/app.js
@@ -4,5 +4,9 @@ const curr = document.querySelector('.js-curr');
 const currParent = curr.parentElement;
 const currGrandParent = currParent.parentElement;
 
-console.log(currParent)
-console.log(currGrandParent)
+const siblingButton = document.createElement('button');
+siblingButton.innerText = 'Usuń z koszyka';
+currParent.appendChild(siblingButton);
+
+console.log(currParent);
+console.log(currGrandParent);
