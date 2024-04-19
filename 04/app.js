00c90493bdccc7be4d4e3d7009471b8d593310b0
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-04-19 16:02:55 +0200

    Task 04 finished

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,18 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const uList = document.createElement('ul');
+console.log(document);
+const navBar = document.querySelector('nav');
+navBar.appendChild(uList);
+
+for(menuItem of menuItems) {
+    console.log(menuItem);
+    const listItem = document.createElement('li');
+    uList.appendChild(listItem);
+    const listItemLink = document.createElement('a');
+    listItem.appendChild(listItemLink);
+    listItemLink.setAttribute('href', menuItem['url']);
+    listItemLink.textContent = menuItem['text'];
+}
