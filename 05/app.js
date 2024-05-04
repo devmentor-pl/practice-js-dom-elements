6f1e09d15aa566f13e3663a4c933d7bc391ca636
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-05-04 12:29:25 +0200

    Task 05.01 completed

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,9 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const deleteFromBasketButton = curr.cloneNode();
+console.log(deleteFromBasketButton)
+
+curr.parentElement.appendChild(deleteFromBasketButton)
+deleteFromBasketButton.innerText = 'Usuń z koszyka'
