7ea718248af6de05cbaf5540742b41e60465294d
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-05-04 21:40:02 +0200

    Task 05.05 completed / so 05 completed

--- 05/app.js
+++ 05/app.js
@@ -2,23 +2,35 @@ console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
 
+// this is for 05.05 - before new elements are added
+// true - deep copy
+const newArticle = curr.parentElement.cloneNode(true)
+
+// 05.01
 const deleteFromBasketButton = curr.cloneNode();
 console.log(deleteFromBasketButton)
 
 curr.parentElement.appendChild(deleteFromBasketButton)
 deleteFromBasketButton.innerText = 'Usuń z koszyka'
 
+// 05.02
 for (const sibling of curr.parentElement.children) {
     if(sibling !== curr) {
         sibling.classList.add('sibling')
     }
 }
 
+// 05.03
 const siblingArticle = curr.parentElement.nextElementSibling
 siblingArticle.setAttribute('title', 'nextElementSibling')
+
+// 05.04
 
 const lastArticle = curr.parentElement.parentElement.lastElementChild
 const newParagraph = document.createElement('p')
 const buttonToReferTo = lastArticle.lastElementChild
 
 lastArticle.insertBefore(newParagraph, buttonToReferTo)
+
+// 05.05
+curr.parentElement.parentElement.insertBefore(newArticle, curr.parentElement)
