b52f163925f5340f42bc2f48489e455a63265773
BartekB94 <wurir1994@gmail.com>
2024-05-17 14:46:07 +0200

    complete 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const liElement = document.querySelector('.comments__item.comments__item--newest');
+
+if(liElement) {
+    const dataAttribute = liElement.querySelectorAll('[data-info');
+    console.log(`${dataAttribute.length}`);
+}
