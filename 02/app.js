7bd659b191c10e81fb69e0f7cbdbd74a2f7557e0
BartekB94 <wurir1994@gmail.com>
2024-05-17 15:41:46 +0200

    complete 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,10 @@
 console.log('DOM');
+
+const liElements = document.querySelectorAll('a[data-url');
+
+if (liElements) {
+    liElements.forEach(function(item) {
+        const dataUrl = item.getAttribute('data-url');
+        item.setAttribute('href', dataUrl);
+    })
+}
