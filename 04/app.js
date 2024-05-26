25ee77e5eb033c9444e4f14a34f642484e4f0f35
Sebastian Pietrzykowski <sebpie84@gmail.com>
2024-05-26 11:59:07 +0200

    exercises 1 - 5

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,63 @@
-console.log('DOM');
+console.log("DOM");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+	{ text: "start", url: "/" },
+	{ text: "galeria", url: "/gallery" },
+	{ text: "kontakt", url: "/contact" },
 ];
+const navList = document.querySelector("nav");
+const ulList = document.createElement("ul");
+navList.append(ulList);
+
+for (let i = 0; i < menuItems.length; i++) {
+
+	const element = menuItems[i];
+	console.log(element);
+	
+	const liList = document.createElement("li");
+	const aLink = document.createElement("a");
+	aLink.textContent = element.text;
+
+	aLink.setAttribute('href', element.url)
+	console.log(liList);
+	liList.append(aLink)
+	ulList.append(liList)
+
+
+}
+
+
+
+// const liListOne = document.createElement("li");
+// const liListTwo = document.createElement("li");
+// const liListThree = document.createElement("li");
+
+// const aLinkOne = document.createElement("a");
+// const alinkTwo = document.createElement("a");
+// const alinkThree = document.createElement("a");
+
+// const href = document.createElement("href");
+// const hrefTwo = document.createElement("href");
+// const hrefThree = document.createElement("href");
+
+
+
+
+// ulList.append(liListOne, liListTwo, liListThree);
+
+// liListOne.append(aLinkOne);
+// liListTwo.append(alinkTwo);
+// liListThree.append(alinkThree);
+
+// aLinkOne.append(href);
+// alinkTwo.append(hrefTwo);
+// alinkThree.append(hrefThree);
+
+// aLinkOne.setAttribute("href", "/");
+// alinkTwo.setAttribute("href", "/gallery");
+// alinkThree.setAttribute("href", "/contact");
+
+// aLinkOne.textContent = "start";
+// alinkTwo.textContent = "galeria";
+// alinkThree.textContent = "kontakt";
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,40 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+const para = document.createElement('p')
+const firstArticle = document.createElement('article')
+firstArticle.closest('section')
+
+console.log(firstArticle);
+
+const btn = document.createElement('button')
+curr.parentElement.append(btn)
+
+const siblingOne = curr.previousElementSibling
+const siblingTwo = siblingOne.previousElementSibling
+
+const article = curr.parentElement.nextElementSibling
+
+
+
+article.setAttribute('title', 'nextElementSibling')
+const copyArticle = article.children
+console.log(copyArticle);
+// copyArticle.append
+
+const paragraph = article.parentElement.lastElementChild.append(para)
+
+
+
+
+
+
+
+
+btn.textContent = 'usun z koszyka'
+btn.className = 'btn'
+para.textContent = 'tu jest ten paragraf'
+
+
+siblingOne.className = 'siblings'
+siblingTwo.className = 'siblings'
