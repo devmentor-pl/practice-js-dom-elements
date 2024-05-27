1bdd496aab9c85a1bc7ff282edc273b66b84cc43
Sebastian Pietrzykowski <sebpie84@gmail.com>
2024-05-27 17:02:56 +0200

    exercises no 5 done

--- 05/app.js
+++ 05/app.js
@@ -1,40 +1,34 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
-const para = document.createElement('p')
-const firstArticle = document.createElement('article')
-firstArticle.closest('section')
-
-console.log(firstArticle);
-
 const btn = document.createElement('button')
+
 curr.parentElement.append(btn)
 
-const siblingOne = curr.previousElementSibling
-const siblingTwo = siblingOne.previousElementSibling
-
-const article = curr.parentElement.nextElementSibling
-
-
-
-article.setAttribute('title', 'nextElementSibling')
-const copyArticle = article.children
-console.log(copyArticle);
-// copyArticle.append
-
-const paragraph = article.parentElement.lastElementChild.append(para)
-
-
-
-
-
-
-
-
 btn.textContent = 'usun z koszyka'
-btn.className = 'btn'
-para.textContent = 'tu jest ten paragraf'
+
+const children = Array.from(curr.parentElement.children)
+
+children.forEach(function(item) {
+    if (item !== curr) {
+        item.classList.add('siblings')
+    }
+} );
 
 
-siblingOne.className = 'siblings'
-siblingTwo.className = 'siblings'
+
+
+const siblingElement = curr.parentElement.nextElementSibling
+
+siblingElement.setAttribute('title', ' nextElementSibling')
+
+const LastArticle = curr.parentElement.nextElementSibling.nextElementSibling
+
+const btnTwo = LastArticle.children[LastArticle.children.length -1]
+
+const pElement = document.createElement('p')
+pElement.textContent = 'tu jest ten paragraf'
+LastArticle.insertBefore(pElement, btnTwo)
+
+const newArticle = curr.parentElement.cloneNode(true)
+curr.parentElement.parentElement.insertBefore(newArticle, curr.parentElement)
